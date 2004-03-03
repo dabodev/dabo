a4cc82c42fa31f14aac4bb705afb975362ca0b23
Paul McNett <p@ulmcnett.com>
2004-03-03 05:18:24 +0000

    Continue...

--- db/__init__.py
+++ db/__init__.py
@@ -1,1 +1,5 @@
+''' dabo.db : The lowest tier, db access. This is where the communication
+              to and from the backend database happens, and cursors get
+              generated and manipulated by the bizobj's in dabo.biz
+'''
 from db import Db
