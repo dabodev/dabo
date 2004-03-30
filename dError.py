746a5fdfee941c54226202484e9f93286f6c231d
Ed Leafe <ed@leafe.com>
2004-03-30 21:05:35 +0000

    removed the tabs and replaced with those horrible spaces

--- dError.py
+++ dError.py
@@ -1,6 +1,6 @@
 
 class dError(StandardError):
-	"""
-	Base class in the framework for passing exceptions.
-	"""
-	pass
+    """
+    Base class in the framework for passing exceptions.
+    """
+    pass
