aaa90fc9ec2f005d8aa2d339562739740e26ad3f
Ed Leafe <ed@leafe.com>
2004-04-22 20:53:28 +0000

    Corrected indentation in the docstring for the class.

--- common/doDefaultMixin.py
+++ common/doDefaultMixin.py
@@ -2,18 +2,18 @@ import sys
 
 
 class DoDefaultMixin(object):
-		''' An alternative way to call superclass method code.
-		
-		Mix this class in to your classes, and you can now use the following
-		form to call superclass methods:
-		
-			retval = cls.doDefault([args])
-		
-		instead of the usual:
-		
-			retval = super(cls, self).<methodName>([args])
-		'''
+	''' An alternative way to call superclass method code.
 	
+	Mix this class in to your classes, and you can now use the following
+	form to call superclass methods:
+	
+		retval = cls.doDefault([args])
+	
+	instead of the usual:
+	
+		retval = super(cls, self).<methodName>([args])
+	'''
+
 	def doDefault(cls, *args, **kwargs):
 		frame = sys._getframe(1)
 		self = frame.f_locals['self']
