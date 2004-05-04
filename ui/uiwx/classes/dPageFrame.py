e9b5368cdbcb5325ae1b6007ae92ce3b7db50974
Paul McNett <p@ulmcnett.com>
2004-05-04 02:21:20 +0000

    Add PageClass and PageCount properties to dPageFrame, for specifying the number of pages (new pages will be based on PageClass, allowing the user to override with their own base class). User still has the option to use AddPage() directly.
    Reworked the setting of the default number of pages... does that work
    for you, knowing that we aren't encouraging passing values to the
    constructors? The only drawback to setting a positive number of pages
    is that user code will have to first set to zero if they want to add
    their own subclassed pages.
    
    Add TabPosition to the list of properties that require recreation.

--- ui/uiwx/classes/dPageFrame.py
+++ ui/uiwx/classes/dPageFrame.py
@@ -5,8 +5,7 @@ class dPageFrame(wx.Notebook, dControlMixin.dControlMixin):
 	""" Create a container for an unlimited number of pages.
 	"""
 	def __init__(self, parent, id=-1, pos=wx.DefaultPosition,
-			size=wx.DefaultSize, style=0, name='dPageFrame',
-			pages=7):
+			size=wx.DefaultSize, style=0, name='dPageFrame'):
 
 		self._baseClass = dPageFrame
 
@@ -18,12 +17,11 @@ class dPageFrame(wx.Notebook, dControlMixin.dControlMixin):
 		self.PostCreate(pre)
 		
 		dControlMixin.dControlMixin.__init__(self, name)
+		
+		self.PageCount = 3
+		
 		self._afterInit()                      # defined in dPemMixin
 		
-		if pages:
-			for i in range(0, pages):
-				self.AddPage(dPage.dPage(self), "Page " + str(i+1) )
-	
 	
 	def afterInit(self):
 		self.lastSelection = 0
@@ -50,6 +48,39 @@ class dPageFrame(wx.Notebook, dControlMixin.dControlMixin):
 		self.lastSelection = cs
 
 	# property get/set functions:
+	def _getPageClass(self):
+		try:
+			return self._pemObject._pageClass
+		except AttributeError:
+			return dPage.dPage
+			
+	def _setPageClass(self, value):
+		if issubclass(value, dControlMixin.dControlMixin):
+			self.pemObject._pageClass = value
+		else:
+			raise TypeError, 'PageClass must descend from a Dabo base class.'
+			
+			
+	def _getPageCount(self):
+		return int(self._pemObject.GetPageCount())
+		
+	def _setPageCount(self, value):
+		value = int(value)
+		pageCount = self._pemObject.GetPageCount()
+		pageClass = self.PageClass
+		
+		if value < 0:
+			raise ValueError, "Cannot set PageCount to less than zero."
+		
+		if value > pageCount:
+			for i in range(pageCount, value):
+				self._pemObject.AddPage(pageClass(self), "Page %s" % (i+1,))
+		elif value < pageCount:
+			for i in range(pageCount, value, -1):
+				self._pemObject.DeletePage(i-1)
+				self._pemObject.Refresh()
+		
+		
 	def _getTabPosition(self):
 		if self.hasWindowStyleFlag(wx.NB_BOTTOM):
 			return 'Bottom'
@@ -83,6 +114,19 @@ class dPageFrame(wx.Notebook, dControlMixin.dControlMixin):
 						"'Top', 'Left', 'Right', and 'Bottom'")
 
 	# Property definitions:
+	PageClass = property(_getPageClass, _setPageClass, None,
+						'Specifies the class of control to use for pages by default. (classRef) \n'
+						'\n'
+						'This really only applies when using the PageCount property to set the\n'
+						'number of pages. If you instead use AddPage() you still need to send \n'
+						'an instance as usual. Class must descend from a dabo base class.')
+						
+	PageCount = property(_getPageCount, _setPageCount, None, 
+						'Specifies the number of pages in the pageframe. (int) \n'
+						'\n'
+						'When using this to increase the number of pages, PageClass \n'
+						'will be queried as the object to use as the page object.')
+						
 	TabPosition = property(_getTabPosition, _setTabPosition, None, 
 						'Specifies where the page tabs are located. (int) \n'
 						'    Top (default) \n'
