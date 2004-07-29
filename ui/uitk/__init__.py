55c4ab9f5a78c0a6535711b2523390700097e6f0
Paul McNett <p@ulmcnett.com>
2004-07-29 17:25:03 +0000

    Begin work abstracting tkinter ui.

--- ui/uitk/__init__.py
+++ ui/uitk/__init__.py
@@ -1,1 +1,4 @@
+from uiApp import uiApp
+from classes import *
+
 uiType = {'shortName': 'tk', 'moduleName': 'uitk', 'longName': 'Tkinter'}
---
+++ ui/uitk/uiApp.py
@@ -0,0 +1,7 @@
+import sys, os, tkinter
+import dabo
+import dabo.ui as ui
+
+# First step is to figure out how to wrap a tk application object if there is one.
+# When that is done, copy/paste/modify from uiwx/uiApp.py.
+class uiApp(): pass
