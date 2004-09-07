8ca95245fb4049d884b0030b05dfeeec49837eb9
Paul McNett <p@ulmcnett.com>
2004-09-07 02:49:39 +0000

    Implemented sqlite per the documentation I've read. I haven't tested this yet however.

--- db/dbSqlite.py
+++ db/dbSqlite.py
@@ -6,48 +6,49 @@ class Sqlite(dBackend):
 		dBackend.__init__(self)
 		self.dbModuleName = "sqlite"
 
+		
 	def getConnection(self, connectInfo):
 		return None
-#		## TO DO 
-# 		import sqlite as dbapi
-# 
-# 		return dbapi.connect(host=connectInfo.Host, 
-# 							user=connectInfo.User,
-# 							passwd=connectInfo.revealPW(),
-# 							db=connectInfo.DbName)
+ 		import sqlite as dbapi
+ 		
+		# sqlite databases are local, either completely in memory
+		# or a local file. Whatever gets passed as the db parameter
+		# will be used as the database, whether it already exists
+		# or not. Pass ":memory" for an in-memory database with no
+		# disk presence at all.
+ 		return dbapi.connect(db=connectInfo.DbName)
 
+		
 	def getDictCursorClass(self):
 		return None
 #		## TO DO 
 # 		import dbapi.cursors as cursors
 # 		return cursors.DictCursor
 
+
 	def getCursor(self, cursorClass):
 		return self._connection.cursor()
 
+		
 	def formatDateTime(self, val):
 		""" We need to wrap the value in quotes. """
 		sqt = "'"		# single quote
 		return "%s%s%s" % (sqt, str(val), sqt)
 		
+		
 	def getLastInsertID(self, cursor):
-		return None
-#		## TO DO 
+		cursor.execute("select last_insert_rowid() as lastid")
+		return cursor.fetchall()[0][0]
 
+		
 	def beginTransaction(self, cursor):
 		""" Begin a SQL transaction."""
-		pass
-#		## TO DO 
-# 		cursor.execute("BEGIN TRANSACTION")
+ 		cursor.execute("BEGIN")
 
 	def commitTransaction(self, cursor):
 		""" Commit a SQL transaction."""
-		pass
-#		## TO DO 
-# 		cursor.execute("COMMIT TRANSACTION")
+ 		cursor.execute("COMMIT")
 
 	def rollbackTransaction(self, cursor):
 		""" Roll back (revert) a SQL transaction."""
-		pass
-#		## TO DO 
-# 		cursor.execute("ROLLBACK TRANSACTION")
+ 		cursor.execute("ROLLBACK")
