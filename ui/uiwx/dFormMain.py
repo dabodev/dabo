ba275b51d063e8a03c918212f716dd82580bc05a
Paul McNett <p@ulmcnett.com>
2004-10-15 03:48:04 +0000

    Cleaned up the code for the background icon in dFormMain: got rid of the bgpanel because it was covering up any controls that the user would add and it wasn't necessary. Just draw to dFormMain's DC.

--- ui/uiwx/dFormMain.py
+++ ui/uiwx/dFormMain.py
@@ -1,7 +1,7 @@
 """ dFormMain.py """
 import wx
 import dFormMixin as fm
-import dPanel, dIcons
+import dPanel, dIcons, dSizer
 import dabo
 
 import time
@@ -23,62 +23,14 @@ else:
 	wxFrameClass = wx.Frame
 	wxPreFrameClass = wx.PreFrame
 
-
-class bgImgPanel(dPanel.dPanel):
-	def __init__(self, parent):
-		bgImgPanel.doDefault(parent)
-		self.bitmap = dIcons.getIconBitmap("dabo_lettering_250x100")
-		self.needRedraw = False
-		self.szTimer = wx.StopWatch()
-		self.bindEvent(dabo.dEvents.Paint, self.onPaint)
-		self.bindEvent(dabo.dEvents.Idle, self.onIdle)
-		self.bindEvent(dabo.dEvents.Resize, self.onResize)
-
-		## To get the Dabo Icon looking right, this was the easiest, but
-		## we'll want to research using a mask in the future.
-		self.BackColor = "White"
-
-	def onPaint(self, evt):
-		dc = wx.PaintDC(self)
-		self.draw(dc)
-
-	def onResize(self, evt):
-		self.needRedraw = True
-		self.szTimer.Start()
-	
-	def onIdle(self, evt):
-		if self.needRedraw:
-			if self.szTimer.Time() < 100:
-				return
-			dc = wx.ClientDC(self)
-			self.draw(dc)
-			self.needRedraw = False
-			self.szTimer.Pause()
-
-	def draw(self, dc):
-		sw = wx.StopWatch()
-		sw.Start()
-		# This throws a non-fatal error, but I don't know why - 
-		# the docs claim that this is perfectly legitimate.
-		wd,ht = dc.GetSize()
-		# bitmap is 128x128, so compute the scale.
-		#dc.SetUserScale( (wd/ 128.0), (ht / 128.0) )
-		## pkm: the scaling is ugly and is what was causing the slow
-		## response: instead, let's center the bitmap.
-		dc.Clear()
-		dc.DrawBitmap(self.bitmap, 10, (ht - 110))
-		sw.Pause()
-
-
 class dFormMain(wxFrameClass, fm.dFormMixin):
 	""" This is the main top-level form for the application.
 	"""
 	def __init__(self):
-
 		self._baseClass = dFormMain
 
 		pre = wxPreFrameClass()
-		self._beforeInit(pre)				   # defined in dPemMixin
+		self._beforeInit(pre)
 		pre.Create(None, -1, "dFormMain")
 
 		self.PostCreate(pre)
@@ -89,23 +41,53 @@ class dFormMain(wxFrameClass, fm.dFormMixin):
 
 		fm.dFormMixin.__init__(self)
 		
-		# Experimental! Places a background image on the main 
-		# form. It's slow and the image looks terrible, but it's a start...
-		if True:
-			self.bg = bgImgPanel(self)
-			self.bg.SetPosition( (0,0) )
-			self.bg.SetSize( self.GetSize() )
-
 		if wx.Platform != '__WXMAC__':
 			self.CreateStatusBar()
 
-		self._afterInit()					   # defined in dPemMixin
+		# Go figure: in order to get rid of the automatic sizing on forms,
+		# you gotta set the sizer to something other than None!
+		self.Sizer = dSizer.dSizer("vertical")
+		self.Sizer.layout()
 
+		self._afterInit()
 		
 	def afterInit(self):
+		dFormMain.doDefault()
 		self.Caption = "Dabo"
 		self.setStatusText("Welcome to Dabo!")
+		
+		# This is to accomodate the Dabo icon, which has a white background.
+		# We should set the white as transparent and set a mask, though.
+		self.BackColor = "White"
+		
+		# Set up the Dabo icon
+		self.bitmap = dIcons.getIconBitmap("dabo_lettering_250x100")
+		self.needRedraw = False
+		self.szTimer = wx.StopWatch()
+		self.bindEvent(dabo.dEvents.Paint, self.__onPaint)
+		self.bindEvent(dabo.dEvents.Idle, self.__onIdle)
+		self.bindEvent(dabo.dEvents.Resize, self.__onResize)
 
+	def __onPaint(self, evt):
+		dc = wx.PaintDC(self)
+		self.draw(dc)
+
+	def __onResize(self, evt):
+		self.needRedraw = True
+		self.szTimer.Start()
+	
+	def __onIdle(self, evt):
+		if self.needRedraw:
+			if self.szTimer.Time() < 100:
+				return
+			self.szTimer.Pause()
+			dc = wx.ClientDC(self)
+			self.draw(dc)
+			self.needRedraw = False
+
+	def draw(self, dc):
+		wd,ht = dc.GetSize()
+		dc.DrawBitmap(self.bitmap, 10, (ht - 110))
 
 if __name__ == "__main__":
 	import test
