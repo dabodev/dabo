30bf5f2b3a85b7cfab11c91adfcb5e0f496125bc
Paul McNett <p@ulmcnett.com>
2004-11-02 22:34:39 +0000

    dbPostgreSQL.py contributed by John Fabiani. Thanks John!

--- db/dbPostgreSQL.py
+++ db/dbPostgreSQL.py
@@ -1,32 +1,42 @@
 from dBackend import dBackend
 import datetime
-import psycopg
 
-class MySQL(dBackend):
+class Postgres(dBackend):
 	def __init__(self):
 		dBackend.__init__(self)
+		#- jfcs 11/01/04 I have to use alpha/beta of psycopg (currently 1.99.10)
+		#- because the 1.1.x series does not support a cursor class (confirmed
+		#- by the author of the module).
 		#self.dbModuleName = "pgdb"
+		#self.dbModuleName = "PgSQL"
 		self.dbModuleName = "psycopg"
 		self.useTransactions = True
 
 	def getConnection(self, connectInfo):
+		### TODO: what connector should we use?
 		import psycopg as dbapi
-
-		port = connectInfo.Port
+		#from pyPgSQL import PgSQL as dbapi
+		
+		#- jfcs 11/01/04 port needs to be a string
+		port = str(connectInfo.Port)
 		if not port:
-			port = 5432
+			port = '5432'
 				
-		self._connection = dbapi.connect(host=connectInfo.Host, 
-				dbname=connectInfo.DbName,
-				user = connectInfo.User,
-				password = connectInfo.revealPW(),
-				port=port)
+		DSN = "host=%s port=%s dbname=%s user=%s password=%s" % (connectInfo.Host,
+			port,
+			connectInfo.DbName,
+			connectInfo.User,
+			connectInfo.revealPW())
+			
+		self._connection = dbapi.connect(DSN)
 		return self._connection
 
 	def getDictCursorClass(self):
-		# Note: this still needs to be tested! Right now we're
-		# just guessing the name of the class in the module.
-		return psycopg.Cursor
+		### TODO: If PostgreSQL doesn't offer specific Dict cursors, 
+		###   return a plain one, and Dabo will convert it.
+		# the new psycopg 2.0 supports DictCursor
+		import psycopg.extras as cursors
+		return cursors.DictCursor 
 
 	def escQuote(self, val):
 		### TODO: This method needs to escape any 'dangerous' characters,
@@ -44,17 +54,17 @@ class MySQL(dBackend):
 		return "%s%s%s" % (sqt, str(val), sqt)
 		
 	def getTables(self, includeSystemTables=False):
-		# MySQL doesn't have system tables, in the traditional sense, as 
-		# they exist in the mysql database.
 		tempCursor = self._connection.cursor()
+		### TODO: Verify that this is the correct syntax
+		# jfcs 11/01/04 assumed public schema
 		tempCursor.execute("select tablename from pg_tables where schemaname = 'public'")
 		rs = tempCursor.fetchall()
 		tables = []
 		for record in rs:
 			tables.append(record[0])
+
 		return tuple(tables)
 		
-
 	def getTableRecordCount(self, tableName):
 		tempCursor = self._connection.cursor()
 		### TODO: Verify syntax
@@ -64,19 +74,55 @@ class MySQL(dBackend):
 	def getFields(self, tableName):
 		tempCursor = self._connection.cursor()
 		### TODO: Verify syntax
-		tempCursor.execute("describe %s" % tableName)
+		#jfcs 11/01/04 works great from psql (but does not work with the psycopg
+		#module) and only with postgres 7.4.x and later.  Too bad, the statement
+		#does everything in one shot.
+		
+		#tempCursor.execute("SELECT c.column_name AS fieldname, c.data_type AS fieldtype, \
+		#i.indisprimary AS is_pkey \
+		#FROM information_schema.columns c \
+		#LEFT JOIN information_schema.key_column_usage cu \
+		#ON (c.table_name=cu.table_name AND c.column_name=cu.column_name) \
+		#LEFT JOIN pg_class cl ON(cl.relname=cu.table_name) \
+		#LEFT JOIN pg_index i ON(cl.oid= i.indrelid) WHERE c.table_name= %s" % tablename)
+		
+		# jfcs 11/01/04 Below sucks but works with 7.3.x and 7.4.x (don't know anything
+		# about 8.0.x) 
+		# Ok get the 'field name', 'field type'
+		tempCursor.execute("Select c.oid,a.attname, t.typname \
+		from pg_class c inner join pg_attribute a \
+		on a.attrelid = c.oid inner join pg_type t on a.atttypid = t.oid \
+		where c.relname = '%s' and a.attnum > 0 " % tableName)
 		rs = tempCursor.fetchall()
+		myoid=rs[0][0]
+		# get the PK 
+		tempCursor.execute("SELECT c2.relname, i.indisprimary, i.indisunique, pg_catalog.pg_get_indexdef(i.indexrelid) \
+		FROM pg_catalog.pg_class c, pg_catalog.pg_class c2, pg_catalog.pg_index i \
+		WHERE c.oid = %s AND c.oid = i.indrelid AND i.indexrelid = c2.oid \
+		AND i.indisprimary =TRUE \
+		ORDER BY i.indisprimary DESC, i.indisunique DESC, c2.relname" % myoid)	
+		rs2=tempCursor.fetchall()
+		if rs2==[]:
+			thePKFieldName=False
+		else:
+			thestr=rs2[0][3]
+			thePKFieldName=thestr[thestr.find('(')+1:thestr.find(')')]
 		
 		fields = []
 		### TODO: Verify the field type names returned.
 		for r in rs:
-			name = r[0]
-			fldType = r[1]
+			name = r[1]
+			if name == thePKFieldName:
+				pk = True
+			else:
+			        pk = False
+				
+			fldType = r[2]
 			if 'int' in fldType:
 				fldType = 'I'
 			elif 'char' in fldType :
 				fldType = 'C'
-			elif 'longtext' in fldType:
+			elif 'text' in fldType:
 				fldType = 'M'
 			elif 'decimal' in fldType:
 				fldType = 'N'
@@ -86,7 +132,7 @@ class MySQL(dBackend):
 				fldType = 'D'
 			else:
 				fldType = "?"
-			pk = (r[3] == 'PRI')
+			
 			
 			fields.append((name.strip(), fldType, pk))
 			
@@ -96,6 +142,8 @@ class MySQL(dBackend):
 
 	def beginTransaction(self, cursor):
 		""" Begin a SQL transaction."""
+		#- jfcs 11/01/04 not sure of this. Normally Postgres is in the 
+		#- transaction mode always????
 		if self.useTransactions:
 			cursor.execute("BEGIN")
 
