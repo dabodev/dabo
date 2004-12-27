cfeb447cccb63a0b9fecfffd28573eee6a37676b
Ed Leafe <ed@leafe.com>
2004-12-27 13:32:56 +0000

    Started re-vamping the way menus are built. The main change is that dMenu has an 'append()' method that will accept a function as a parameter. If a function is passed, the class will bind the EVT_MENU for that menuitem to that function, simplifying the coding.

--- ui/uiwx/dMenu.py
+++ ui/uiwx/dMenu.py
@@ -2,10 +2,47 @@
 import wx
 import dPemMixin as pm
 
+# wx constants for styles
+dNormalItem = wx.ITEM_NORMAL
+dCheckItem =  wx.ITEM_CHECK
+dRadioItem = wx.ITEM_RADIO
+
 class dMenu(wx.Menu, pm.dPemMixin):
+	_IsContainer = False
+	
 	def __init__(self, mainForm=None):
+		self.mainForm = mainForm
 		if mainForm:
-			self.mainForm = mainForm
 			self.actionList = mainForm.Application.actionList
-		#dMenu.doDefault()
+		else:
+			self.actionList = None
 		super(dMenu, self).__init__()
+
+	def appendSeparator(self):
+		self.AppendSeparator()
+	
+	def appendMenu(self, prompt, menu, help=""):
+		self.AppendMenu(-1, prompt, menu, helpStr=help)
+		
+	def append(self, prompt, func=None, hlp="", menutype=""):
+		itmtyp = self.getItemType(menutype)
+		self.Append(-1, prompt, help=hlp, kind=itmtyp)
+		if func:
+			self.Bind(wx.EVT_MENU, func)
+	
+	def insert(self, pos, prompt, help="", type=""):
+		itmtyp = self.getItemType(typ)
+		self.Insert(-1, pos, prompt, helpString=help, kind=itmtyp)
+	
+	def getItemType(self, typ):
+		typ = str(typ).lower()[:3]
+		ret = dNormalItem
+		if typ in ("che", "chk"):
+			ret = dCheckItem
+		elif typ == "rad":
+			# Currently only implemented under Windows and GTK, 
+			# use #if wxHAS_RADIO_MENU_ITEMS to test for 
+			# availability of this feature.
+			ret = dRadioItem
+		return ret
+
--- ui/uiwx/dMenuBar.py
+++ ui/uiwx/dMenuBar.py
@@ -2,4 +2,23 @@
 import wx
 
 class dMenuBar(wx.MenuBar):
-	pass
+	_IsContainer = False
+	
+	def __init__(self, frm=None):
+		super(dMenuBar, self).__init__()
+		self._parent = frm
+		self._menus = {}
+
+	def append(self, menu, prompt):
+		self.Append(menu, prompt)
+		self._menus[prompt] = menu
+	
+	def insert(self, pos, menu, prompt):
+		self.Insert(pos, menu, prompt)
+		self._menus[prompt] = menu
+
+	def getMenu(self, cap):
+		ret = None
+		if self._menus.has_key(cap):
+			ret = self._menus[cap]
+		return ret
