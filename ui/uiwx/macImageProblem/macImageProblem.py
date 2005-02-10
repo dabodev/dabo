38aeada4561a0ec68564f0f8f60a7f38452cfff1
Paul McNett <p@ulmcnett.com>
2005-02-10 18:57:42 +0000

    Modified the test so I can follow up with wxPython-mac.

--- ui/uiwx/macImageProblem/macImageProblem.py
+++ ui/uiwx/macImageProblem/macImageProblem.py
@@ -12,7 +12,8 @@ pf = wx.Notebook(form)
 
 il = wx.ImageList(16, 16, initialCount=0)
 
-for image in ("checkMark.png", "browse.png", "edit.png"):
+for image in ("checkMark.png", "browse.png", "edit.png", 
+              "checkMark1.png", "edit1.png"):
 	print "Adding image '%s'..." % image
 	bmp = getIconBitmap(image)
 	print "bmp ok?:", bmp.Ok()
@@ -23,5 +24,10 @@ pf.AssignImageList(il)
 pf.AddPage(wx.Panel(pf), "select", imageId=0)
 pf.AddPage(wx.Panel(pf), "browse", imageId=1)
 pf.AddPage(wx.Panel(pf), "edit", imageId=2)
+pf.AddPage(wx.Panel(pf), "select 1", imageId=3)
+pf.AddPage(wx.Panel(pf), "edit 1", imageId=4)
+
+
+
 form.Show()
 app.MainLoop()
