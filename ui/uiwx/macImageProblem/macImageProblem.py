8dac4960448e69e8787c6b5458693d7f4dfb42e7
Paul McNett <p@ulmcnett.com>
2005-02-10 19:22:37 +0000

    Fsck'd up on that last change, so I reduced the test down to just the old and new edit icons.

--- ui/uiwx/macImageProblem/macImageProblem.py
+++ ui/uiwx/macImageProblem/macImageProblem.py
@@ -12,8 +12,7 @@ pf = wx.Notebook(form)
 
 il = wx.ImageList(16, 16, initialCount=0)
 
-for image in ("checkMark.png", "browse.png", "edit.png", 
-              "checkMark1.png", "edit1.png"):
+for image in ("edit.png", "edit1.png"): 
 	print "Adding image '%s'..." % image
 	bmp = getIconBitmap(image)
 	print "bmp ok?:", bmp.Ok()
@@ -21,11 +20,8 @@ for image in ("checkMark.png", "browse.png", "edit.png",
 pf.AssignImageList(il)
 
 
-pf.AddPage(wx.Panel(pf), "select", imageId=0)
-pf.AddPage(wx.Panel(pf), "browse", imageId=1)
-pf.AddPage(wx.Panel(pf), "edit", imageId=2)
-pf.AddPage(wx.Panel(pf), "select 1", imageId=3)
-pf.AddPage(wx.Panel(pf), "edit 1", imageId=4)
+pf.AddPage(wx.Panel(pf), "grey alpha mask", imageId=0)
+pf.AddPage(wx.Panel(pf), "black alpha mask", imageId=1)
 
 
 
