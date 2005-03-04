169c6eeba94e451bbf35c89655ad7a75766ecef8
Paul McNett <p@ulmcnett.com>
2005-03-04 20:59:22 +0000

    further test

--- ui/uiwx/macMouseEventProblem/wxClick.py
+++ ui/uiwx/macMouseEventProblem/wxClick.py
@@ -4,6 +4,7 @@ class TestFrame(wx.Frame):
 	def __init__(self, parent, id):
 		super(TestFrame, self).__init__(parent=parent, id=id)
 		self.SetTitle("WX")
+		self.SetSizer(wx.BoxSizer(wx.VERTICAL))
 		self.SetSize((300, 400))
 		self.Bind(wx.EVT_LEFT_DOWN, self.onMDown)
 		self.Bind(wx.EVT_LEFT_UP, self.onMUp)
@@ -20,7 +21,6 @@ class TestFrame(wx.Frame):
 if __name__ == '__main__':
 	app = wx.PySimpleApp()
 	frm = TestFrame(None, -1)
-	# OK, now we have to tell Python to show the form
-	# Shouldn't this be by default?
+	but = wx.Button(frm, -1, "t")
 	frm.Show(1)
 	app.MainLoop()
