b62378e71a52c1fee45d0e7c98e3dec92dbb4e05
Paul McNett <p@ulmcnett.com>
2005-08-03 00:11:55 +0000

    Moved reporting_old to reporting.

--- dReportWriter.py
+++ dReportWriter.py
@@ -1,4 +1,4 @@
-from dabo.lib.reporting_old.reportWriter import ReportWriter
+from dabo.lib.reporting.reportWriter import ReportWriter
 from dabo.common.dObject import dObject
 
 # dReportWriter is simply a raw ReportWriter/dObject mixin:
