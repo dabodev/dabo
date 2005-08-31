b0a68bb8356c018866d89c8c4df8b440c650909e
Paul McNett <p@ulmcnett.com>
2005-08-31 18:02:45 +0000

    This fixes Dabo's __version__.py to accurately reflect Subversion's revision number, if the client is using Dabo as a Subversion working copy. Previously, if you were to check out an older revision, the revision number wouldn't reflect that.

--- __version__.py
+++ __version__.py
@@ -1,6 +1,30 @@
-from dabo.lib._daborevs import _revs
-_revision = _revs["dabo"]
+import os
+import dabo
+
+# If dabo is a subversion working copy, we'll try to get the current
+# revision using the svnversion command against that working copy.
+# Else, we'll get the revision as saved in _daborevs, which is an
+# external svn repository updated by a svn post-commit hook on the
+# server. 
+daboPath = dabo.__path__[0]
+_revision = None
+olddir = os.getcwd()
+
+if os.path.exists(os.path.join(daboPath, ".svn")):
+	try:
+		os.chdir(daboPath)
+		_revision = os.popen("svnversion .").read().strip()
+	except:
+		pass
+	os.chdir(olddir)
+
+if _revision is None:
+	from dabo.lib._daborevs import _revs
+	_revision = _revs["dabo"]
 
 version = {"version": "0.4.1",
            "revision": _revision}
 
+if __name__ == "__main__":
+	print "Dabo Version: %s \n    Revision: %s" % (
+		version["version"], version["revision"])
