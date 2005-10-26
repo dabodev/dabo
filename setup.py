0d63e281cff23e963e0d0021dc826efd5de76379
Paul McNett <p@ulmcnett.com>
2005-10-26 22:30:08 +0000

    Converted a couple whole files from space indentation to tab indentation, and made the tool executable.

--- setup.py
+++ setup.py
@@ -7,21 +7,21 @@ from dabo.__version__ import version
 daboVersion = version["version"]
 
 setup(
-    name = "Dabo",
-    version = daboVersion,
-    url = 'http://dabodev.com/',
-    author = 'Ed Leafe and Paul McNett',
-    author_email = 'dev@dabodev.com',
-    description = 'Dabo 3-tier Application Framework',
-    license = 'MIT',
-    packages = find_packages(),
-    package_data = {
-        '':['ANNOUNCE', 'AUTHORS', 'ChangeLog', 'INSTALL',
-'LICENSE.TXT', 'README', 'TODO'],
-        'dabo.icons': ['*.png'],
-        'dabo.lib.reporting':['*.rfxml'],
-        'dabo.lib.reporting_stefano':['*.rfxml'],
-        'dabo.ui.uiwx.macImageProblem':['*.png'],
-        'dabo.ui.uiwx.masked':['README'],
-    },
+		name = "Dabo",
+		version = daboVersion,
+		url = 'http://dabodev.com/',
+		author = 'Ed Leafe and Paul McNett',
+		author_email = 'dev@dabodev.com',
+		description = 'Dabo 3-tier Application Framework',
+		license = 'MIT',
+		packages = find_packages(),
+		package_data = {
+				'':['ANNOUNCE', 'AUTHORS', 'ChangeLog', 'INSTALL',
+				'LICENSE.TXT', 'README', 'TODO'],
+				'dabo.icons': ['*.png'],
+				'dabo.lib.reporting':['*.rfxml'],
+				'dabo.lib.reporting_stefano':['*.rfxml'],
+				'dabo.ui.uiwx.macImageProblem':['*.png'],
+				'dabo.ui.uiwx.masked':['README'],
+		},
 )
