70d3106dd8bdcb22d29af1daf68e8c053b960bac
Paul McNett <p@ulmcnett.com>
2005-12-01 19:48:45 +0000

    Made the looking for icons cross-platform

--- dabo/icons/__init__.py
+++ dabo/icons/__init__.py
@@ -1,15 +1,15 @@
-""" dabo.icons """
-import sys, os
+import sys
+import os
 
 def getIcon(iconName):
 	return open("%s/%s.png" % (__path__[-1:][0], iconName), 'rb').read()
 
 def getIconFileName(iconName):
-	ret = "%s/%s.png" % (__path__[-1:][0], iconName)
+	ret = os.path.join(__path__[-1:][0], "%s.png" % iconName)
 	
 	if not os.path.exists(ret):
 		for pth in sys.path:
-			icn = "%s/%s.png" % (pth, iconName)
+			icn = os.path.join(pth, "%s.png" % iconName)
 			if os.path.exists(icn):
 				ret = icn
 				break	
