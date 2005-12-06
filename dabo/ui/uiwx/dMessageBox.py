a006aadbac8c562fd6e5152f1696b064f57c7808
Paul McNett <p@ulmcnett.com>
2005-12-06 18:31:09 +0000

    Made the dMessageBox titles come from Application.getAppInfo(appName) by default.

--- dabo/ui/uiwx/dMessageBox.py
+++ dabo/ui/uiwx/dMessageBox.py
@@ -5,6 +5,8 @@ along with convenience functions to allow calling like:
 	if dAreYouSure("Delete this record"):
 """
 import wx
+import dabo
+
 
 def getForm():
 		mainForm = wx.GetApp().GetTopWindow()
@@ -24,8 +26,10 @@ class dMessageBox(wx.MessageDialog):
 		wx.MessageDialog.__init__(self, form, message, title, style)
 
 
-def areYouSure(message="Are you sure?", title="Dabo",
+def areYouSure(message="Are you sure?", title=None,
 			defaultNo=False, cancelButton=True):
+	if title is None:
+		title = getDefaultTitle()
 	style = wx.YES_NO|wx.ICON_QUESTION
 	if cancelButton:
 		style = style|wx.CANCEL
@@ -44,15 +48,21 @@ def areYouSure(message="Are you sure?", title="Dabo",
 		return None
 
 
-def stop(message="Stop", title="Dabo"):
+def stop(message="Stop", title=None):
+	if title is None:
+		title = getDefaultTitle()
 	icon = wx.ICON_HAND
 	showMessageBox(message=message, title=title, icon=icon)
 
-def info(message="Information", title="Dabo"):
+def info(message="Information", title=None):
+	if title is None:
+		title = getDefaultTitle()
 	icon = wx.ICON_INFORMATION
 	showMessageBox(message=message, title=title, icon=icon)
 
-def exclaim(message="Important!", title="Dabo"):
+def exclaim(message="Important!", title=None):
+	if title is None:
+		title = getDefaultTitle()
 	icon = wx.ICON_EXCLAMATION
 	showMessageBox(message=message, title=title, icon=icon)
 
@@ -64,13 +74,28 @@ def showMessageBox(message, title, icon):
 	return None
 
 
-def info(message="Information", title="Dabo"):
+def info(message="Information", title=None):
+	if title is None:
+		title = getDefaultTitle()
 	style = wx.OK|wx.ICON_INFORMATION
 	dlg = dMessageBox(message, title, style)
 	retval = dlg.ShowModal()
 	dlg.Destroy()
 	return None
 
+
+def getDefaultTitle():
+	"""Get a default title for the messageboxes.
+
+	Comes from app.getAppInfo("appName"), or if the application
+	object isn't available the title will be "Dabo Application".
+	"""
+	ret = None
+	if dabo.dAppRef:
+		ret = dabo.dAppRef.getAppInfo("appName")
+	if ret is None:
+		ret = "Dabo Application"
+	return ret
 	
 
 if __name__ == "__main__":
