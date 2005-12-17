6e4ac312b7ddbecdb82ea5139ff77f8056edbbf8
Ed Leafe <ed@leafe.com>
2005-12-17 19:10:01 +0000

    Added a significant change to eventMixin.py's auto event binding logic. The code now first looks at the object itself before looking up its family tree. This is critical for the event binding when running a cdxml file, where methods are added to the object at runtime, and are not present in the class definition.
    Cleaned up some spacing in dEvents; also fixed some "appliesToClass" logic that had events 'applying' to incorrect classses. There are still some anomalies, I'm sure; the event listing in the Designer will reveal these.

--- dabo/dEvents.py
+++ dabo/dEvents.py
@@ -3,6 +3,8 @@ import dabo
 from dabo.dObject import dObject
 import dabo.ui as ui
 import dabo.biz.dBizobj as dBizobj
+from dabo.dLocalize import _
+
 
 class Event(dObject):
 	""" Base class for Dabo events.
@@ -25,7 +27,6 @@ class Event(dObject):
 		self._args = args
 		self._kwargs = kwargs
 		self._continue = True
-		
 		self._baseClass = Event
 		
 		self._insertEventData()
@@ -52,8 +53,7 @@ class Event(dObject):
 		
 		
 	def _insertEventData(self):
-		""" Place ui-specific stuff into the ui-agnostic EventData dictionary.
-		"""
+		""" Place ui-specific stuff into the ui-agnostic EventData dictionary."""
 		eventData = {}		
 		nativeEvent = self._uiEvent
 		kwargs = self._kwargs
@@ -76,8 +76,7 @@ class Event(dObject):
 				
 			
 	def _logEvent(self):
-		""" Log the event if the event object's LogEvents property is set.
-		"""
+		""" Log the event if the event object's LogEvents property is set."""
 		eventName = self.__class__.__name__
 		
 		try:
@@ -94,9 +93,11 @@ class Event(dObject):
 		if eventName not in noLogEvents:		
 			for logEventName in logEvents:
 				if logEventName.lower() == "all" or logEventName == eventName:
-					dabo.infoLog.write("dEvent Fired: %s %s" % (self._eventObject.getAbsoluteName(), 
-						self.__class__.__name__,))
+					dabo.infoLog.write("dEvent Fired: %s %s" % 
+							(self._eventObject.getAbsoluteName(), 
+							self.__class__.__name__,))
 					break
+
 
 	def __getattr__(self, att):
 		if self.EventData.has_key(att):
@@ -104,11 +105,13 @@ class Event(dObject):
 		raise AttributeError, "%s.%s object has no attribute %s." % (
 			self.__class__.__module__, self.__class__.__name__, att)
 			
+			
 	def _getContinue(self):
 		return self._continue
 		
 	def _setContinue(self, val):
 		self._continue = bool(val)
+		
 		
 	def _getEventObject(self):
 		return self._eventObject
@@ -116,27 +119,32 @@ class Event(dObject):
 	def _setEventObject(self, obj):
 		self._eventObject = obj
 	
+	
 	def _getEventData(self):
 		return self._eventData
 		
 	def _setEventData(self, dict):
 		self._eventData = dict
 	
+	
 	Continue = property(_getContinue, _setContinue, None,
-		"Specifies whether the event is allowed to continue on to the next handler.")
+			_("""Specifies whether the event is allowed to continue 
+			on to the next handler.  (bool)"""))
 		
 	EventObject = property(_getEventObject, _setEventObject, None, 
-		"References the object that emitted the event.")
+			_("References the object that emitted the event.  (obj)"""))
 		
 	EventData = property(_getEventData, _setEventData, None,
-		"Dictionary of data name/value pairs associated with the event.")
+			_("""Dictionary of data name/value pairs associated 
+			with the event.  (dict)"""))
 		
 
 class DataEvent(Event):
 	def appliesToClass(eventClass, objectClass):
 		return issubclass(objectClass, dabo.biz.dBizobj)
 	appliesToClass = classmethod(appliesToClass)
-			
+		
+		
 class EditorEvent(Event):
 	def appliesToClass(eventClass, objectClass):
 		return issubclass(objectClass, dabo.ui.dEditor)
@@ -147,54 +155,66 @@ class GridEvent(Event):
 		return issubclass(objectClass, dabo.ui.dGrid)
 	appliesToClass = classmethod(appliesToClass)
 	
+	
 class KeyEvent(Event):
 	def appliesToClass(eventClass, objectClass):
 		return issubclass(objectClass, dabo.ui.dPemMixin)
 	appliesToClass = classmethod(appliesToClass)
+	
 	
 class ListEvent(Event):
 	def appliesToClass(eventClass, objectClass):
 		return issubclass(objectClass, (dabo.ui.dListControl, dabo.ui.dListBox))
 	appliesToClass = classmethod(appliesToClass)
 	
+
 class MenuEvent(Event):
 	def appliesToClass(eventClass, objectClass):
 		return issubclass(objectClass, (dabo.ui.dMenu, dabo.ui.dMenuItem,
 				dabo.ui.dMenuBar))
 	appliesToClass = classmethod(appliesToClass)
 	
+
 class MouseEvent(Event):
 	def appliesToClass(eventClass, objectClass):
 		return issubclass(objectClass, dabo.ui.dPemMixin)
 	appliesToClass = classmethod(appliesToClass)
 	
+
 class SashEvent(Event):
 	def appliesToClass(eventClass, objectClass):
 		return issubclass(objectClass, dabo.ui.dSplitter)
 	appliesToClass = classmethod(appliesToClass)
+
 
 class TreeEvent(Event):
 	def appliesToClass(eventClass, objectClass):
 		return issubclass(objectClass, dabo.ui.dTreeView)
 	appliesToClass = classmethod(appliesToClass)
 
+
 class Activate(Event):
 	"""Occurs when the form or application becomes active."""
 	def appliesToClass(eventClass, objectClass):
-		return issubclass(objectClass, (dabo.dApp, dabo.ui.dForm, dabo.ui.dFormMain, dabo.ui.dDialog))
+		return issubclass(objectClass, (dabo.dApp, dabo.ui.dForm,
+				dabo.ui.dFormMain, dabo.ui.dDialog))
 	appliesToClass = classmethod(appliesToClass)
 	
+
 class Close(Event):
 	"""Occurs when the user closes the form."""
 	def appliesToClass(eventClass, objectClass):
-		return issubclass(objectClass, (dabo.ui.dForm, dabo.ui.dFormMain, dabo.ui.dDialog))
+		return issubclass(objectClass, (dabo.ui.dForm, dabo.ui.dFormMain,
+				dabo.ui.dDialog))
 	appliesToClass = classmethod(appliesToClass)
+	
 	
 class Create(Event):
 	"""Occurs after the control or form is created."""
 	def appliesToClass(eventClass, objectClass):
 		return issubclass(objectClass, dabo.ui.dPemMixin)
 	appliesToClass = classmethod(appliesToClass)
+	
 	
 class ChildBorn(Event):
 	"""Occurs when a child control is created."""
@@ -205,34 +225,46 @@ class ChildBorn(Event):
 			self.Child = None
 		super(ChildBorn, self).__init__(*args, **kwargs)
 	def appliesToClass(eventClass, objectClass):
-		return issubclass(objectClass, dabo.ui.dPemMixin)
+		return issubclass(objectClass, (dabo.ui.dForm, dabo.ui.dDialog,
+				dabo.ui.dPanel, dabo.ui.dPage, dabo.ui.dPageFrame,
+				dabo.ui.dPageList, dabo.ui.dPageSelect, dabo.ui.dPageFrameNoTabs))
 	appliesToClass = classmethod(appliesToClass)
 
+
 class ContextMenu(MenuEvent):
-	"""Occurs when the user requests a context menu (right-click on Win, opt-click on Mac, etc."""
+	"""Occurs when the user requests a context menu (right-click on Win,
+	control-click on Mac, etc.
+	"""
 	pass
 	
+
 class Deactivate(Event):
 	"""Occurs when another form becomes active."""
 	def appliesToClass(eventClass, objectClass):
-		return issubclass(objectClass, (dabo.dApp, dabo.ui.dForm, dabo.ui.dFormMain, dabo.ui.dDialog))
+		return issubclass(objectClass, (dabo.dApp, dabo.ui.dForm,
+				dabo.ui.dFormMain, dabo.ui.dDialog))
 	appliesToClass = classmethod(appliesToClass)
 	
+
 class Destroy(Event):
 	"""Occurs when the control or form is destroyed."""
 	def appliesToClass(eventClass, objectClass):
 		return issubclass(objectClass, dabo.ui.dPemMixin)
 	appliesToClass = classmethod(appliesToClass)
 	
+
 class Hit(Event):
-	"""Occurs with the control's default event (button click, listbox pick, checkbox, etc.)"""
+	"""Occurs with the control's default event (button click, 
+	listbox pick, checkbox, etc.)
+	"""
 	def appliesToClass(eventClass, objectClass):
-		return issubclass(objectClass, (ui.dBitmapButton, ui.dButton, ui.dCheckBox,
-			ui.dComboBox, ui.dDropdownList, ui.dEditBox, ui.dListBox,
-			ui.dRadioGroup, ui.dSlider, ui.dSpinner, ui.dTextBox,
-			ui.dTimer, ui.dToggleButton, ui.dMenuItem))
+		return issubclass(objectClass, (ui.dBitmapButton, ui.dButton,
+				ui.dCheckBox, ui.dComboBox, ui.dDropdownList, ui.dEditBox,
+				ui.dListBox, ui.dRadioGroup, ui.dSlider, ui.dSpinner, ui.dTextBox,
+				ui.dTimer, ui.dToggleButton, ui.dMenuItem))
 	appliesToClass = classmethod(appliesToClass)
 	
+
 class Idle(Event):
 	"""Occurs when the event loop has no active events to process.
 	
@@ -244,6 +276,7 @@ class Idle(Event):
 		return issubclass(objectClass, dabo.ui.dPemMixin)
 	appliesToClass = classmethod(appliesToClass)
 	
+
 class GotFocus(Event):
 	"""Occurs when the control gets the focus."""
 	def appliesToClass(eventClass, objectClass):
@@ -252,13 +285,17 @@ class GotFocus(Event):
 
 	
 class KeyChar(KeyEvent):
-	"""Occurs when a key is depressed and released on the focused control or form."""
+	"""Occurs when a key is depressed and released on the 
+	focused control or form.
+	"""
 	pass
 	
+
 class KeyDown(KeyEvent):
 	"""Occurs when any key is depressed on the focused control or form."""
 	pass
 	
+
 class KeyUp(KeyEvent):
 	"""Occurs when any key is released on the focused control or form."""
 	pass
@@ -292,64 +329,87 @@ class MouseEnter(MouseEvent):
 	"""Occurs when the mouse pointer enters the form or control."""
 	pass
 	
+
 class MouseLeave(MouseEvent): 
 	"""Occurs when the mouse pointer leaves the form or control."""
 	pass
+
 
 class MouseMove(MouseEvent):
 	"""Occurs when the mouse moves in the control."""
 	pass
 	
+
 class MouseWheel(MouseEvent):
 	"""Occurs when the user scrolls the mouse wheel."""
 	pass
 	
+
 class MouseLeftDown(MouseEvent):
 	"""Occurs when the mouse's left button is depressed on the control."""
 	pass
 	
+
 class MouseLeftUp(MouseEvent):
 	"""Occurs when the mouse's left button is released on the control."""
 	pass
 	
+
 class MouseLeftClick(MouseEvent):
-	"""Occurs when the mouse's left button is depressed and released on the control."""
+	"""Occurs when the mouse's left button is depressed 
+	and released on the control.
+	"""
 	pass
 	
+
 class MouseLeftDoubleClick(MouseEvent):
 	"""Occurs when the mouse's left button is double-clicked on the control."""
 	pass
 	
+
 class MouseRightDown(MouseEvent):
 	"""Occurs when the mouse's right button is depressed on the control."""
 	pass
 	
+
 class MouseRightUp(MouseEvent):
 	"""Occurs when the mouse's right button is released on the control."""
 	pass
 
+
 class MouseRightClick(MouseEvent):
-	"""Occurs when the mouse mouse's right button is depressed and released on the control."""
+	"""Occurs when the mouse mouse's right button is depressed 
+	and released on the control.
+	"""
 	pass
 	
+
 class MouseRightDoubleClick(MouseEvent):
 	"""Occurs when the mouse's right button is double-clicked on the control."""
 	pass
 	
+
 class MouseMiddleDown(MouseEvent):
 	"""Occurs when the mouse's middle button is depressed on the control."""
 	pass
 	
+
 class MouseMiddleUp(MouseEvent):
 	"""Occurs when the mouse's middle button is released on the control."""
 	pass
 
+
 class MouseMiddleClick(MouseEvent):
-	"""Occurs when the mouse mouse's middle button is depressed and released on the control."""
+	"""Occurs when the mouse mouse's middle button is depressed
+	and released on the control.
+	"""
 	pass
 	
+
 class MouseMiddleDoubleClick(MouseEvent):
-	"""Occurs when the mouse's middle button is double-clicked on the control."""
+	"""Occurs when the mouse's middle button is double-clicked 
+	on the control.
+	"""
 	pass
 	
 	
@@ -359,6 +419,7 @@ class Paint(Event):
 		return issubclass(objectClass, dabo.ui.dPemMixin)
 	appliesToClass = classmethod(appliesToClass)
 	
+
 class PageChanged(Event):
 	"""Occurs when a page in a pageframe-like control changes"""
 	def appliesToClass(eventClass, objectClass):
@@ -366,12 +427,14 @@ class PageChanged(Event):
 				dabo.ui.dPageSelect, dabo.ui.dPageFrameNoTabs))
 	appliesToClass = classmethod(appliesToClass)
 	
+
 class PageEnter(Event):
 	"""Occurs when the page becomes the active page."""
 	def appliesToClass(eventClass, objectClass):
 		return issubclass(objectClass, dabo.ui.dPage)
 	appliesToClass = classmethod(appliesToClass)
 	
+
 class PageLeave(Event):
 	"""Occurs when a different page becomes active."""
 	def appliesToClass(eventClass, objectClass):
@@ -395,133 +458,166 @@ class SashDoubleClick(SashEvent):
 	"""Occurs when a user double-clicks on the sash of a splitter window."""
 	pass
 
+
 class SashPositionChanged(SashEvent):
 	"""Occurs when a user moves the sash of a splitter window."""
 	pass
+
 
 class ListSelection(ListEvent):
 	""" Occurs when an item is highlighted in a list."""
 	pass
 
+
 class ListDeselection(ListEvent):
 	""" Occurs when a selected item is deselected in a list."""
 	pass
+
 
 class TreeSelection(TreeEvent):
 	""" Occurs when the selected item in a tree control changes."""
 	pass
 
+
 class TreeItemCollapse(TreeEvent):
 	""" Occurs when an expanded item in a tree collapses."""
 	pass
+
 
 class TreeItemExpand(TreeEvent):
 	""" Occurs when a collapsed item in a tree expands."""
 	pass
 
+
 class GridContextMenu(GridEvent, MenuEvent):
 	"""Occurs when the context menu is requested in the grid region."""
 	pass
+
 
 class GridHeaderContextMenu(GridEvent, MenuEvent):
 	"""Occurs when the context menu is requested in the grid header region."""
 	pass
 
+
 class GridHeaderIdle(GridEvent):
 	"""Occurs when an idle cycle happens in the grid header."""
 	pass
+
 
 class GridHeaderMouseEnter(GridEvent, MouseEvent):
 	"""Occurs when the mouse pointer enters the grid's header region."""
 	pass
 
+
 class GridHeaderMouseLeave(GridEvent, MouseEvent):
 	"""Occurs when the mouse pointer leaves the grid's header region."""
 	pass
+
 
 class GridHeaderMouseLeftClick(GridEvent, MouseEvent):
 	"""Occurs when the left mouse button is clicked in the header region."""
 	pass
 
+
 class GridHeaderMouseLeftDoubleClick(GridEvent, MouseEvent):
 	"""Occurs when the left mouse button is double-clicked in the header region."""
 	pass
+
 
 class GridHeaderMouseLeftDown(GridEvent, MouseEvent):
 	"""Occurs when the left mouse button goes down in the header region."""
 	pass
 
+
 class GridHeaderMouseLeftUp(GridEvent, MouseEvent):
 	"""Occurs when the left mouse button goes up in the header region."""
 	pass
+
 
 class GridHeaderMouseRightClick(GridEvent, MouseEvent):
 	"""Occurs when the right mouse button is clicked in the header region."""
 	pass
 
+
 class GridHeaderMouseRightDown(GridEvent, MouseEvent):
 	"""Occurs when the left mouse button goes down in the header region."""
 	pass
+
 
 class GridHeaderMouseRightUp(GridEvent, MouseEvent):
 	"""Occurs when the left mouse button goes up in the header region."""
 	pass
 
+
 class GridHeaderMouseMove(GridEvent, MouseEvent):
 	"""Occurs when the mouse moves in the grid header region."""
 	pass
+
 
 class GridHeaderPaint(GridEvent):
 	"""Occurs when it's time to paint the grid header."""
 	pass
 
+
 class GridMouseLeftClick(GridEvent, MouseEvent):
 	"""Occurs when the left mouse button is clicked in the grid region."""
 	pass
+
 
 class GridMouseLeftDoubleClick(GridEvent, MouseEvent):
 	"""Occurs when the left mouse button is double-clicked in the grid region."""
 	pass
 
+
 class GridMouseLeftDown(GridEvent, MouseEvent):
 	"""Occurs when the left mouse button goes down in the grid region."""
 	pass
+
 
 class GridMouseLeftUp(GridEvent, MouseEvent):
 	"""Occurs when the left mouse button goes up in the grid region."""
 	pass
 
+
 class GridMouseRightClick(GridEvent, MouseEvent):
 	"""Occurs when the right mouse button is clicked in the header region."""
 	pass
+
 
 class GridMouseRightDown(GridEvent, MouseEvent):
 	"""Occurs when the right mouse button goes down in the grid region."""
 	pass
 
+
 class GridMouseRightUp(GridEvent, MouseEvent):
 	"""Occurs when the right mouse button goes up in the grid region."""
 	pass
+
 
 class GridMouseMove(GridEvent, MouseEvent):
 	"""Occurs when the mouse moves in the grid region (not the headers)."""
 	pass
 
+
 class GridRowSize(GridEvent):
 	"""Occurs when the grid's rows are resized."""
 	pass
+
 
 class GridCellSelected(GridEvent):
 	"""Occurs when the a new cell is selected in the grid."""
 	pass
 
+
 class GridCellEdited(GridEvent):
 	"""Occurs when the user edits the content of a grid cell."""
 	pass
 
+
 class GridColSize(GridEvent):
 	"""Occurs when the grid's columns are resized."""
 	pass
+
 
 class DocumentationHint(EditorEvent):
 	"""Occurs when the editor wants documentation information to change.
@@ -546,11 +642,14 @@ class TitleChanged(EditorEvent):
 
 
 class ValueChanged(Event):
-	"""Occurs when the control's value has changed, whether programmatically or interactively."""
+	"""Occurs when the control's value has changed, whether
+	programmatically or interactively.
+	"""
 	def appliesToClass(eventClass, objectClass):
 		return issubclass(objectClass, dabo.ui.dDataControlMixin)
 	appliesToClass = classmethod(appliesToClass)
 	
+
 class ValueRefresh(Event):
 	"""Occurs when the form wants the controls to refresh their values."""
 	def appliesToClass(eventClass, objectClass):
