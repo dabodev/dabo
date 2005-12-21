67df1aba1b4ac1b8bc2017286a029e5ef2debae2
Adi J. Sieker <ml@adsworth.info>
2005-12-21 21:08:05 +0000

    in dDialog.show, if the return value of Show or ShowModal isn't in the retVal dict return whatever Show or ShowModal returned. This allows devs to pass their own values to EndModal and not just wx.ID_OK or wx.ID_CANCEL.

--- ui/uiwx/dDialog.py
+++ ui/uiwx/dDialog.py
@@ -56,7 +56,7 @@ class dDialog(wx.Dialog, fm.dFormMixin):
 			ret = self.ShowModal()
 		else:
 			ret = self.Show(True)
-		return retVals.get(ret)
+		return retVals.get(ret, ret)
 		
 
 	def hide(self):
