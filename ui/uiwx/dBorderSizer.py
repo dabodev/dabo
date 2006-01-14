5917bfe0d77ecac2e5f690a9c907154904a05dcf
Ed Leafe <ed@leafe.com>
2006-01-14 17:16:18 +0000

    Updated the long-neglected BorderSizer class, which wraps the wx.StaticBoxSizer class. You can now pass either a pre-made dBox object to the constructor, or pass a parent object, and the constructor will create the box for you.

--- ui/uiwx/dBorderSizer.py
+++ ui/uiwx/dBorderSizer.py
@@ -1,16 +1,27 @@
 import wx
 import dabo
-import dPemMixin
-import dSizerMixin
+from dabo.dLocalize import _
 
-class dBorderSizer(wx.StaticBoxSizer, dSizerMixin.dSizerMixin):
-	"""TODO: Explain what a BorderSizer does and when you'd use it."""
+dabo.ui.loadUI("wx")
+
+
+class dBorderSizer(wx.StaticBoxSizer, dabo.ui.dSizerMixin):
+	"""A BorderSizer is a regular box sizer, but with a visible box around
+	the perimiter. You must either create the box first and pass it to the 
+	dBorderSizer's constructor, or pass a parent object, and the box
+	will be created for you in the constructor as a child object of the parent
+	you passed.
+	"""
 	def __init__(self, box, orientation="h"):
 		# Make sure that they got the params in the right order
 		if isinstance(box, basestring):
 			box, orientation = orientation, box
-		if not isinstance(box, wx.StaticBox):
-			raise dException.dException, "Must pass an instance of a box to dBorderSizer"
+		if not isinstance(box, dabo.ui.dBox):
+			try:
+				prnt = box
+				box = dabo.ui.dBox(prnt)
+			except:
+				raise dException.dException, "Must pass an instance of dBox or a parent object to dBorderSizer"
 		# Convert Dabo orientation to wx orientation
 		if orientation[0].lower() == "v":
 			orientation = wx.VERTICAL
@@ -18,8 +29,35 @@ class dBorderSizer(wx.StaticBoxSizer, dSizerMixin.dSizerMixin):
 			orientation = wx.HORIZONTAL
 		wx.StaticBoxSizer.__init__(self, box, orientation)
 	
-		
-if __name__ == "__main__":
-	s = dBorderSizer()
-	
 
+	def _getBox(self):
+		return self.GetStaticBox()
+
+	Box = property(_getBox, None, None,
+			_("Reference to the box used in the sizer  (dBox)"))
+
+
+	
+class TestForm(dabo.ui.dForm):
+	def afterInit(self):
+		self.Sizer = dabo.ui.dSizer("v", Border=10)
+		lbl = dabo.ui.dLabel(self, Caption="Button in BoxSizer Below", FontSize=16)
+		self.Sizer.append(lbl, halign="center")
+		sz = dBorderSizer(self, "v")
+		self.Sizer.append1x(sz)
+		btn = dabo.ui.dButton(self, Caption="Click")
+		sz.append1x(btn)
+		pnl = dabo.ui.dPanel(self, BackColor="seagreen")
+		self.Sizer.append1x(pnl, border=18)
+		
+class _dBorderSizer_test(dBorderSizer):
+	def __init__(self, *args, **kwargs):
+		super(_dBorderSizer_test, self).__init__(box=bx, orientation="h", *args, **kwargs)
+
+
+
+
+if __name__ == "__main__":
+	app = dabo.dApp()
+	app.MainFormClass = TestForm
+	app.start()
