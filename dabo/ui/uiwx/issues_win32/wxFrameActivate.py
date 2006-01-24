e2c6beb8fc03e8b6817cffbcdaecbd3344ce5635
Paul McNett <p@ulmcnett.com>
2006-01-24 21:10:20 +0000

    Confirmed double-activate event on windows; added a pure-wx test, and will send to wxPython-dev.

---
+++ dabo/ui/uiwx/issues_win32/wxFrameActivate.py
@@ -0,0 +1,17 @@
+import wx
+
+print "On Windows, note the double Activate events..."
+
+app = wx.PySimpleApp()
+
+def onActivate(evt):
+	if evt.GetActive():
+		print "Activate"
+	else:
+		print "Deactivate"
+
+frame = wx.Frame(None, -1)
+frame.Bind(wx.EVT_ACTIVATE, onActivate)
+
+frame.Show()
+app.MainLoop()
