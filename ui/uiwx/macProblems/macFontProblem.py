50e58e4aec77bbe43036122ce58ccaf2dfe4fb8c
Paul McNett <p@ulmcnett.com>
2006-03-20 22:44:54 +0000

    I put together a wx sample that mirrors what dabo does when changing font properties. I'm going to test the behavior on Win and Linux and then post a query to wxPython-mac.

---
+++ ui/uiwx/macProblems/macFontProblem.py
@@ -0,0 +1,46 @@
+import wx
+
+class TestFrame(wx.Frame):
+	def __init__(self, parent, id):
+		super(TestFrame, self).__init__(parent=parent, id=id)
+		self.SetTitle("wx Font BoldItalic test")
+		self.SetSize((300, 400))
+
+		self.tx1 = wx.TextCtrl(self, -1)
+		self.tx1.SetPosition( (20,20) )
+		self.tx1.Name = "Text1"
+
+		but = wx.Button(self, -1, "test")
+		but.Bind(wx.EVT_BUTTON, self.onBut)
+
+	def onBut(self, evt):
+		self.setFont()
+
+	def setFont(self):
+		print "----button click----"
+		font = self.tx1.GetFont()
+		if font.GetWeight() == wx.FONTWEIGHT_NORMAL:
+			print "font is not bold; set to bold"
+			font.SetWeight(wx.FONTWEIGHT_BOLD)
+			self.tx1.SetFont(font)
+			return
+
+		print "font was already bold"
+
+		if font.GetStyle() == wx.FONTSTYLE_NORMAL:
+			print "font is not italic; set to italic"
+			font.SetStyle(wx.FONTSTYLE_ITALIC)
+			self.tx1.SetFont(font)
+			return
+
+		print "font was already italic"
+
+		
+		
+
+if __name__ == '__main__':
+	app = wx.PySimpleApp()
+	frm = TestFrame(None, -1)
+	frm.Show(1)
+	app.MainLoop()
+
