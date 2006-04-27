6878ccf5983056de5fdb53902a3a99831185de07
Echo <oshecho@gmail.com>
2006-04-27 21:06:46 +0000

    Fixed a bug.

--- dabo/biz/dAutoBizobj.py
+++ dabo/biz/dAutoBizobj.py
@@ -30,7 +30,7 @@ def setupAutoBiz(conn, autoBizes):
 	"""
 	for obj in autoBizes:
 		t = obj(conn)
-		
+
 
 def autoCreateTables(noAccessDialog=None):
 	"""This function creates tables if they don't exist.
@@ -38,29 +38,29 @@ def autoCreateTables(noAccessDialog=None):
 	property is set for a dAutoBizobj.
 	"""
 	if len(g._AutoTables) == 0:
-		raise dException.dException(_("No tables have been setup for autocreation."))	
-		
+		raise dException.dException(_("No tables have been setup for autocreation."))
+
 	g._toExc = {}
 	for biz in g._AutoTables.values():
-		ac = biz.AutoCommit 
+		ac = biz.AutoCommit
 		biz.AutoCommit = True
 		biz.CreateTable()
-		
+
 		biz.AutoCommit = ac
-		
+
 	if g._toExc:
 		if dabo.dAppRef is not None:
 			class DbAdminLogin(dabo.ui.dDialog):
 				def __init__(self, parent, conn):
 					self._conn = conn
 					self.super(parent)
-				
+
 				def addControls(self):
 					self.Caption = self.Application.getAppInfo("appName")
-					
+
 					self.Sizer = dabo.ui.dSizer("v")
 					cs = dabo.ui.dGridSizer()
-					
+
 					lblmain = self.addObject(dabo.ui.dLabel, Caption=_("The database could not be setup. Contact your DB administrator."), FontBold=True, FontSize=14)
 					lblinst = self.addObject(dabo.ui.dLabel, Caption=_("""For the DB Admin:
  The tables must either created by:
@@ -73,21 +73,21 @@ def autoCreateTables(noAccessDialog=None):
 					cs.append(o, row=0, col=0, border=3)
 					self.txtUsername = self.addObject(dabo.ui.dTextBox)
 					cs.append(self.txtUsername, row=0, col=1, border=3)
-					
+
 					o = self.addObject(dabo.ui.dLabel, Caption=_("Password"))
 					cs.append(o, row=1, col=0, border=3)
 					self.txtPassword = self.addObject(dabo.ui.dTextBox, PasswordEntry=True)
 					cs.append(self.txtPassword, row=1, col=1, border=3)
-					
+
 					s = dabo.ui.dSizer()
 					b = self.addObject(dabo.ui.dButton, DefaultButton=True, Caption=_("OK"))
 					b.bindEvent(dabo.dEvents.Hit, self.onHitOK)
 					s.append(b, border=3)
-					
+
 					b = self.addObject(dabo.ui.dButton, CancelButton=True, Caption=_("Cancel"))
 					b.bindEvent(dabo.dEvents.Hit, self.onHitCancel)
 					s.append(b, border=3)
-					
+
 					self.Sizer.append(lblmain, border=3, halign="center")
 					self.Sizer.append(lblinst, border=3)
 					self.Sizer.append(lblinst2, border=3)
@@ -95,30 +95,30 @@ def autoCreateTables(noAccessDialog=None):
 					self.Sizer.append(cs, halign="center")
 					self.Sizer.appendSpacer(10, 10)
 					self.Sizer.append(s, halign='center')
-					
+
 				def onHitOK(self, evt):
 					if not self.txtUsername.Value:
 						dabo.ui.exclaim(_("You must enter the username first."))
 						return
-					
+
 					if not self.txtPassword.Value:
 						dabo.ui.exclaim(_("You must enter the password first."))
 						return
-					
+
 					self._data = (self.txtUsername.Value, self.txtPassword.Value)
 					self.Accepted = True
 					self.EndModal(dabo.dConstants.DLG_OK)
-			
+
 				def onHitCancel(self, evt):
 					self._data = ()
 					self.Accepted = False
 					self.EndModal(dabo.dConstants.DLG_CANCEL)
-					
+
 				def _getAnswer(self):
 					return self._data
-					
+
 				Answer = property(_getAnswer)
-			
+
 			for k in g._toExc.keys():
 				if noAccessDialog is None:
 					login = DbAdminLogin(None, k)
@@ -130,7 +130,7 @@ def autoCreateTables(noAccessDialog=None):
 					user = login.Answer[0]
 					password = login.Answer[1]
 					login.release()
-					
+
 					#Temporarly connect to the database using the new user and pass
 					ci = dabo.db.dConnectInfo(DbType=k.ConnectInfo.DbType, Database=k.ConnectInfo.Database, Host=k.ConnectInfo.Host, User=user, PlainTextPassword=password)
 					try:
@@ -141,7 +141,7 @@ def autoCreateTables(noAccessDialog=None):
 						raise dException.DBNoAccessException
 					else:
 						cur = tempConn.getDaboCursor()
-						
+
 						#Execute the needed queries
 						for query in g._toExc[k]:
 							try:
@@ -150,12 +150,12 @@ def autoCreateTables(noAccessDialog=None):
 								dabo.ui.stop(_("Could not setup the database. Access was denied."))
 								_WriteQueriesToFile(g._toExc)
 								raise dException.DBNoAccessException
-					
+
 				else:
 					login.release()
 					_WriteQueriesToFile(g._toExc)
 					raise dException.DBNoAccessException
-							
+
 		else:
 			_WriteQueriesToFile(g._toExc)
 			raise dException.DBNoAccessException
@@ -167,16 +167,16 @@ def _WriteQueriesToFile(queries):
 		f.write(_("#Queries for DB '%s' on host '%s':\n") % (k.ConnectInfo.Database, k.ConnectInfo.Host))
 		for query in queries[k]:
 			f.write("%s;\n" % (query))
-			
+
 	f.close
-		
+
 class dAutoBizobj(dBizobj):
 	"""This class is just like bBizobj but is supports the auto creation of
 	the table by setting the table property.
-	
+
 	If there is a field that is set to AutoIncrement, self.AutoPopulatePK
 	is set to true.
-	If there is a field that is a stamp field, it is set to not update that field."""		
+	If there is a field that is a stamp field, it is set to not update that field."""
 	def _beforeInit(self):
 		self.super()
 		self._table = None
@@ -185,18 +185,18 @@ class dAutoBizobj(dBizobj):
 
 	def _afterInit(self):
 		table = self.getTable()
-		if table is not None:			
+		if table is not None:
 			self._table = table
 			g._AutoTables[table.Name] = self
-			
+
 			self.DataSource = table.Name
 			self.KeyField = table.PK[0]
 			self.addFrom(table.Name)
 			for fld in table.Fields:
 				self.addField("%s.%s as %s" % (table.Name, fld.Name, fld.Name))
-				
+
 				self.DefaultValues[fld.Name] = fld.Default
-				
+
 				if fld.DataType == "Numeric":
 					self._CurrentCursor._types[fld.Name] = type(int)
 				elif fld.DataType == "Float":
@@ -218,16 +218,15 @@ class dAutoBizobj(dBizobj):
 					self._CurrentCursor._types[fld.Name] = type(datetime.datetime)
 				elif fld.DataType == "Binary":
 					self._CurrentCursor._types[fld.Name] = type(str)
-				
+
 				if fld.IsAutoIncrement:
 					self.AutoPopulatePK = True
-				
+
 				if fld.DataType == "Stamp":
-					self._CurrentCursor.setNonUpdateFields([fld.Name])
-		
+					self.NonUpdateFields = self.NonUpdateFields + [fld.Name]
+
 		self.afterInit()
 
-		
 	def getTable(self):
 		"""Return the dTable definition for the table.
 		"""
@@ -250,11 +249,11 @@ class dAutoBizobj(dBizobj):
 		"""Create the tables that has been asigned to this bizobj."""
 		if self._table is None:
 			raise dException.dException(_("No table has been defined for this bizobj."))
-		
+
 		if self._CurrentCursor.BackendObject.isExistingTable(self.Table.Name):
 			self._table_checked = True
 			return
-			
+
 		#Create table
 		toExc = self._CurrentCursor.BackendObject.createTableAndIndexes(self._table, self._CurrentCursor)
 		if toExc:
@@ -271,7 +270,7 @@ class dAutoBizobj(dBizobj):
 				self.new()
 				for k in to_insert.keys():
 					self.setFieldVal(k, to_insert[k][i])
-				
+
 				try:
 					self.save()
 				except dException.DBQueryException, e:
@@ -284,6 +283,6 @@ class dAutoBizobj(dBizobj):
 	def _getTable(self):
 		return self._table
 
-		
+
 	Table = property(_getTable, None, None,
 			_("The table definition for this bizobj.  (object)"))
