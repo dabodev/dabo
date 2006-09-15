2da72501b6f63d7c006428e4d92e65624b61b8a9
Paul McNett <p@ulmcnett.com>
2006-09-15 15:36:25 +0000

    Made dabo's __version__.py more generic, so the code can be used again with no changes in daboide.

--- __version__.py
+++ __version__.py
@@ -1,31 +1,41 @@
 import os
-import dabo
 
+
+# The _version line is the only thing you should change in this file.
+# Everything else is boilerplate copied also to other dabo repositories.
 _version = "0.7a"
 
-# If dabo is a subversion working copy, we'll try to get the current revision 
-# using the svnversion command against that working copy. Otherwise, we'll get
-# the revision as saved in _daborevs, which is an external svn repository 
-# updated by a svn post-commit hook on the server. 
-daboPath = dabo.__path__[0]
+
+# If the package is a subversion working copy, we'll try to get the current 
+# revision using the svnversion command against that working copy. Otherwise, 
+# we'll get the revision as saved in _daborevs, which is an external svn 
+# repository updated by a svn post-commit hook on the server. 
+
+cwd_split = os.path.split(os.getcwd())
+package_name = cwd_split[-1]
+package_path = ''.join(cwd_split[:-1])
+
 _revision = None
 olddir = os.getcwd()
 
-if os.path.exists(os.path.join(daboPath, ".svn")):
+if os.path.exists(os.path.join(package_path, ".svn")):
 	try:
-		os.chdir(daboPath)
+		os.chdir(package_path)
 		_revision = os.popen("svnversion .").read().strip()
 	except:
 		pass
 	os.chdir(olddir)
 
 if _revision is None:
-	from dabo.lib._daborevs import _revs
-	_revision = _revs["dabo"]
+	from lib._daborevs import _revs
+	_revision = _revs[package_name]
 
 version = {"version": _version,
 		"revision": _revision}
 
 if __name__ == "__main__":
-	print "Dabo Version: %s \n    Revision: %s" % (
-		version["version"], version["revision"])
+	print """
+Package Name: %s
+     Version: %s
+    Revision: %s
+""" % (package_name, version["version"], version["revision"])
