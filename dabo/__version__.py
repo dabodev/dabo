af857616a7769768c5c2db7c7a016e9bc73dc78d
Paul McNett <p@ulmcnett.com>
2006-10-15 00:11:53 +0000

    Noticed that in dabodemo/minesweeper, appRecipes, and montana, the dabo version information was coming from dabodemo, not dabo. This was due to some boneheadedness in setting up getting the version from svn. I elected to simplify it and not try getting the svnversion information, and to just use _daborevs.

--- dabo/__version__.py
+++ dabo/__version__.py
@@ -1,34 +1,16 @@
-import os
-
-
-# The _version line is the only thing you should change in this file.
+# The following 2 lines are the only thing you should change in this file.
 # Everything else is boilerplate copied also to other dabo repositories.
 package_name = "dabo"
 _version = "0.7a"
 
 
-# If the package is a subversion working copy, we'll try to get the current 
-# revision using the svnversion command against that working copy. Otherwise, 
-# we'll get the revision as saved in _daborevs, which is an external svn 
+import os
+from lib._daborevs import _revs
+
+# We'll get the revision as saved in _daborevs, which is an external svn 
 # repository updated by a svn post-commit hook on the server. 
 
-cwd_split = os.path.split(os.getcwd())
-package_path = ''.join(cwd_split[:-1])
-
-_revision = None
-olddir = os.getcwd()
-
-if os.path.exists(os.path.join(package_path, ".svn")):
-	try:
-		os.chdir(package_path)
-		_revision = os.popen("svnversion .").read().strip()
-	except:
-		pass
-	os.chdir(olddir)
-
-if _revision is None:
-	from lib._daborevs import _revs
-	_revision = _revs.get(package_name, "")
+_revision = _revs.get(package_name, "")
 
 version = {"version": _version,
 		"revision": _revision}
