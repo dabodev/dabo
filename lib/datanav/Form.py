040f95d83a3ff6aff01fcdcf9376732dda759518
Ed Leafe <ed@leafe.com>
2007-01-05 21:31:29 +0000

    Forgot the second half of the change. This fixes the wording of the dialog.

--- lib/datanav/Form.py
+++ lib/datanav/Form.py
@@ -438,7 +438,7 @@ class Form(dabo.ui.dForm):
 						"(rfxml) to your application's reports directory? If you say "
 						"'yes', you'll be able to modify the file and it will be used "
 						"as the Quick Report from now on (it will no longer be auto-"
-						"generated). The file will be generated when you click 'OK'."
+						"generated). The file will be generated when you click 'Yes'."
 						"\n\nGenerate the report form file?", cancelButton=False):
 					self.saveNamedReportForm = True
 
