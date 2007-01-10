3f8dd77f675f61afefece4082a1f465630600fdc
Paul McNett <p@ulmcnett.com>
2007-01-10 18:05:39 +0000

    Added flags at the top of the unit tests to easily set which database backends to test against.
    Fixed a problem in my branch's dCursorMixin.isChanged(allRows=False) that 
    wasn't taking into account new records. I found this thanks to the unit testing.

--- db/test/test_dCursorMixin.py
+++ db/test/test_dCursorMixin.py
@@ -2,6 +2,21 @@ import unittest
 import dabo
 
 
+# Testing anything other than sqlite requires network access. So set these
+# flags so that only the db's you want to test against are True.
+test_sqlite = True
+test_mysql = True
+
+if test_sqlite:
+	sqlite_unittest = unittest.TestCase
+else:
+	sqlite_unittest = object
+
+if test_mysql:
+	mysql_unittest = unittest.TestCase
+else:
+	mysql_unittest = object
+
 
 class Test_dCursorMixin(object):
 	def setUp(self):
@@ -27,7 +42,7 @@ class Test_dCursorMixin(object):
 		self.assertEqual(ds[1], ("cField", "C", False, "test", "cField", None))
 
 
-class Test_dCursorMixin_sqlite(Test_dCursorMixin, unittest.TestCase):
+class Test_dCursorMixin_sqlite(Test_dCursorMixin, sqlite_unittest):
 	def setUp(self):
 		con = dabo.db.dConnection(DbType="SQLite", Database=":memory:")
 		self.cur = con.getDaboCursor()
@@ -45,6 +60,34 @@ insert into test (cField, iField, nField) values ("Carl Karsten", 10223, 23032.7
 
 	def tearDown(self):
 		self.cur = None
+
+class Test_dCursorMixin_mysql(Test_dCursorMixin, mysql_unittest):
+	def setUp(self):
+		con = dabo.db.dConnection(DbType="MySQL", User="dabo_unittest", 
+				password="T30T35DB4K30Z45I67N60", Database="dabo_unittest",
+				Host="paulmcnett.com")
+		self.cur = con.getDaboCursor()
+		super(Test_dCursorMixin_mysql, self).setUp()
+
+	def tearDown(self):
+		self.cur.execute("drop table %s" % self.temp_table_name)
+		super(Test_dCursorMixin_mysql, self).tearDown()
+
+	def createSchema(self):
+		cur = self.cur
+		cur.execute("""
+create table %s (pk INTEGER PRIMARY KEY AUTO_INCREMENT, cField CHAR (32), iField INT, nField DECIMAL (8,2))
+""" % self.temp_table_name)
+		cur.execute("""		
+insert into %s (cField, iField, nField) values ("Paul Keith McNett", 23, 23.23)
+""" % self.temp_table_name)
+		cur.execute("""		
+insert into %s (cField, iField, nField) values ("Edward Leafe", 42, 42.42)
+""" % self.temp_table_name)
+		cur.execute("""		
+insert into %s (cField, iField, nField) values ("Carl Karsten", 10223, 23032.76)
+""" % self.temp_table_name)
+
 
 if __name__ == "__main__":
 	unittest.main()
