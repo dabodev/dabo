cf14cb3ff906d6096cf4740bac327f4c0cf7a77f
Paul McNett <p@ulmcnett.com>
2007-01-10 18:22:17 +0000

    Fixed up the trunk's version of the dCursorMixin test. I'll eventually replace this one with the one from my branch, but at least it runs correctly now after my last commit broke it.

--- dabo/db/test/test_dCursorMixin.py
+++ dabo/db/test/test_dCursorMixin.py
@@ -1,5 +1,6 @@
 import unittest
 import dabo
+from dabo.lib import getRandomUUID
 
 
 # Testing anything other than sqlite requires network access. So set these
@@ -21,9 +22,11 @@ else:
 class Test_dCursorMixin(object):
 	def setUp(self):
 		cur = self.cur
-		cur.UserSQL = "select * from test"
+		self.temp_table_name = "unittest%s" % getRandomUUID().replace("-", "")[-20:]
+		self.createSchema()
+		cur.UserSQL = "select * from %s" % self.temp_table_name
 		cur.KeyField = "pk"
-		cur.Table = "test"
+		cur.Table = self.temp_table_name
 		cur.requery()
 
 	def createSchema(self):
@@ -36,30 +39,32 @@ class Test_dCursorMixin(object):
 	def testRecordGetValue(self):
 		self.assertEqual(self.cur.Record.cField, "Paul Keith McNett")
 
-	def testDataStructure(self):
+	def test_DataStructure(self):
 		ds = self.cur.DataStructure
-		self.assertEqual(ds[0], ("pk", "I", True, "test", "pk", None))
-		self.assertEqual(ds[1], ("cField", "C", False, "test", "cField", None))
+		self.assertTrue(ds[0] == ("pk", "I", True, self.temp_table_name, "pk", None)
+				or ds[0] == ("pk", "G", True, self.temp_table_name, "pk", None))
+		self.assertEqual(ds[1], ("cField", "C", False, self.temp_table_name, "cField", None))
+		self.assertTrue(ds[2] == ("iField", "I", False, self.temp_table_name, "iField", None)
+				or ds[2] == ("iField", "G", False, self.temp_table_name, "iField", None))
+		self.assertEqual(ds[3], ("nField", "N", False, self.temp_table_name, "nField", None))
 
 
 class Test_dCursorMixin_sqlite(Test_dCursorMixin, sqlite_unittest):
 	def setUp(self):
 		con = dabo.db.dConnection(DbType="SQLite", Database=":memory:")
 		self.cur = con.getDaboCursor()
-		self.createSchema()
 		super(Test_dCursorMixin_sqlite, self).setUp()
 
 	def createSchema(self):
-		"""Creates the test database schema for the tests."""
 		cur = self.cur
+		tableName = self.temp_table_name
 		cur.executescript("""
-create table test (pk INTEGER PRIMARY KEY AUTOINCREMENT, cField CHAR, iField INT, nField DECIMAL (8,2));
-insert into test (cField, iField, nField) values ("Paul Keith McNett", 23, 23.23);
-insert into test (cField, iField, nField) values ("Edward Leafe", 42, 42.42);
-insert into test (cField, iField, nField) values ("Carl Karsten", 10223, 23032.76);""")
+create table %s (pk INTEGER PRIMARY KEY AUTOINCREMENT, cField CHAR, iField INT, nField DECIMAL (8,2));
+insert into %s (cField, iField, nField) values ("Paul Keith McNett", 23, 23.23);
+insert into %s (cField, iField, nField) values ("Edward Leafe", 42, 42.42);
+insert into %s (cField, iField, nField) values ("Carl Karsten", 10223, 23032.76);
+""" % (tableName, tableName, tableName, tableName, ))
 
-	def tearDown(self):
-		self.cur = None
 
 class Test_dCursorMixin_mysql(Test_dCursorMixin, mysql_unittest):
 	def setUp(self):
