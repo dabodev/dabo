b6b1d73928860e486a83b4cecb5de50962cbba79
Paul McNett <p@ulmcnett.com>
2007-01-12 03:11:56 +0000

    Fixed up the dabo types to include "L", BLOB. Mapped it to str as it will work just fine that way. Could have also used array.array, but it wouldn't have been as easy as array.array() needs a type argument.
    Made sure that the pythonTypes map str and unicode back to the appropriate 
    daboType ("C") instead of one of the other daboTypes that happen to map to
    str or unicode.
    
    Deleted some local names that were cluttering the dabo.db namespace.

--- db/__init__.py
+++ db/__init__.py
@@ -39,19 +39,22 @@ from dNoEscQuoteStr import dNoEscQuoteStr
 from dDataSet import dDataSet
 
 daboTypes = {
-		"C": unicode,
-		"M": unicode,
-		"I": int,
-		"G": long,
-		"F": float,
-		"B": bool,
-		"D": datetime.date,
-		"T": datetime.datetime,
-		"N": Decimal}
+		"C": unicode,             ## text
+		"M": unicode,             ## memo (longtext) 
+		"I": int,                 ## integer
+		"G": long,                ## long integer
+		"F": float,               ## float
+		"B": bool,                ## boolean (logical)
+		"D": datetime.date,       ## date
+		"T": datetime.datetime,   ## datetime
+		"N": Decimal,             ## decimal (numeric)
+		"L": str,                 ## BLOB
+		}
 
 pythonTypes = dict([[v,k] for k,v in daboTypes.iteritems()])
 pythonTypes[str] = "C"
-
+pythonTypes[unicode] = "C"
+del k, v, Decimal
 
 def getPythonType(daboType):
 	"""Given a char type code like "I" or "C", return the associated Python type."""
