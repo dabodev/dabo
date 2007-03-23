20f4f26d2376eb9e68b2603691887222abeaf987
Paul McNett <p@ulmcnett.com>
2007-03-23 00:06:08 +0000

    Been pulling my hair out all afternoon on a dDialog that refused to stay put between hide() and show() calls, only to realize I'm dealing with something in wxPython. Committing this to test on other platforms.

---
+++ dabo/ui/uiwx/winProblems/frame_position_reset.py
@@ -0,0 +1,20 @@
+##- On Gtk, wxPython 2.8.1.1, hiding and showing a wx.Frame resets its
+##- position back to its original. Run this test, and reposition the frame,
+##- and then click inside the frame. It'll jump back to its original position.
+
+import wx
+
+def onClick(evt):
+	frm = evt.GetEventObject()
+	print 1, frm.GetPosition()
+	frm.Show(False)
+	print 2, frm.GetPosition()
+	frm.Show()
+	print 3, frm.GetPosition()
+
+app = wx.App()
+frm = wx.Frame(None)
+frm.Bind(wx.EVT_LEFT_UP, onClick)
+
+frm.Show()
+app.MainLoop()
