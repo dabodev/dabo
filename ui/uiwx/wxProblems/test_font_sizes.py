20cef96e2694a7647c559966a2fa9841644f7d87
Paul McNett <p@ulmcnett.com>
2007-03-24 14:15:43 +0000

    Made a wx script to show buttons in various font sizes. I want to see how they compare platform to platform, as I've notice font sizes on Mac being significantly smaller.

---
+++ ui/uiwx/wxProblems/test_font_sizes.py
@@ -0,0 +1,23 @@
+import sys
+import wx
+
+fontSizes = [int(a) for a in sys.argv[1:]]
+
+if not fontSizes:
+	fontSizes = [8, 10, 12, 14]
+
+app = wx.App()
+frm = wx.Frame(None)
+pan = wx.Panel(frm)
+
+offset = (0,0)
+for fontSize in fontSizes:
+	but = wx.Button(pan, -1, "%s pt" % fontSize)
+	but.SetPosition(offset)
+	font = but.GetFont()
+	font.SetPointSize(fontSize)
+	but.SetFont(font)
+	offset = (offset[0]+10, offset[1]+30)
+
+frm.Show()
+app.MainLoop()
