6e3557990e81b13c0133ce56773582525815db71
Paul McNett <p@ulmcnett.com>
2007-06-06 20:38:37 +0000

    Fix setup.py to include the icon theme directories in the python egg.

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+import glob
 import ez_setup # From http://peak.telecommunity.com/DevCenter/setuptools
 ez_setup.use_setuptools()
 
@@ -5,6 +7,33 @@ from setuptools import setup, find_packages
 from dabo.__version__ import version
 
 daboVersion = version["version"]
+
+# List the paths under dabo/icon/themes:
+iconDir = "dabo/icons/themes"
+iconDirs = {}
+def getIconSubDir(arg, dirname, fnames):
+	if ".svn" not in dirname and "cards" not in dirname.lower() and dirname[-1] != "\\":
+		icons = glob.glob(os.path.join(dirname, "*.png"))
+		if icons:
+			subdir = os.path.join(iconDir, dirname[len(arg)+1:])
+			subdir = subdir.replace(os.sep, ".")
+			iconDirs[subdir] = ["*.png"]
+os.path.walk(iconDir, getIconSubDir, iconDir)
+
+package_data = {
+		'':['ANNOUNCE', 'AUTHORS', 'ChangeLog', 'INSTALL',
+		'LICENSE.TXT', 'README', 'TODO'],
+		'dabo.icons': ['*.png', '*.ico'],
+		'dabo.icons.cards.small': ['*.png', '*.ico'],
+		'dabo.icons.cards.large': ['*.png', '*.ico'],
+		'dabo.lib.reporting':['*.rfxml'],
+		'dabo.lib.reporting_stefano':['*.rfxml'],
+		'dabo.ui.uiwx.macImageProblem':['*.png'],
+		'dabo.ui.uiwx.masked':['README'],
+		}
+
+package_data.update(iconDirs)
+
 
 setup(
 		name = "Dabo",
@@ -15,15 +44,5 @@ setup(
 		description = 'Dabo 3-tier Application Framework',
 		license = 'MIT',
 		packages = find_packages(),
-		package_data = {
-				'':['ANNOUNCE', 'AUTHORS', 'ChangeLog', 'INSTALL',
-				'LICENSE.TXT', 'README', 'TODO'],
-				'dabo.icons': ['*.png', '*.ico'],
-				'dabo.icons.cards.small': ['*.png', '*.ico'],
-				'dabo.icons.cards.large': ['*.png', '*.ico'],
-				'dabo.lib.reporting':['*.rfxml'],
-				'dabo.lib.reporting_stefano':['*.rfxml'],
-				'dabo.ui.uiwx.macImageProblem':['*.png'],
-				'dabo.ui.uiwx.masked':['README'],
-		},
+		package_data = package_data,
 )
