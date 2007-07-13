a85d03b5ef236c5281d23602cf39b1f3a7845f64
Paul McNett <p@ulmcnett.com>
2007-07-13 00:32:49 +0000

    Applied [3242] (ticket #1029) to the stable branch (docstrings for ui.stop and friends).

--- dabo/ui/uiwx/dMessageBox.py
+++ dabo/ui/uiwx/dMessageBox.py
@@ -29,7 +29,20 @@ class dMessageBox(wx.MessageDialog):
 
 def areYouSure(message="Are you sure?", title=None, defaultNo=False, 
 		cancelButton=True, parent=None):
-	"""Display a message dialog asking the user to answer Yes, No to a question."""	
+	"""Display a dMessageBox asking the user to answer yes or no to a question.
+
+	Returns True, False, or None, for choices "Yes", "No", or "Cancel".
+
+	The default title comes from app.getAppInfo("appName"), or if the 
+	application object isn't available it will be "Dabo Application".
+
+	If defaultNo is True, the 'No' button will be the default button.
+
+	If cancelButton is True (default), a third 'Cancel' button will appear.
+
+	If parent isn't passed, it will automatically resolve to the current 
+	active form.
+	"""	
 	if title is None:
 		title = getDefaultTitle()
 	style = wx.YES_NO|wx.ICON_QUESTION
@@ -51,18 +64,48 @@ def areYouSure(message="Are you sure?", title=None, defaultNo=False,
 
 
 def stop(message="Stop", title=None, parent=None):
+	"""Display a dMessageBox informing the user that the operation cannot proceed.
+
+	Returns None.
+
+	The default title comes from app.getAppInfo("appName"), or if the 
+	application object isn't available it will be "Dabo Application".
+
+	If parent isn't passed, it will automatically resolve to the current 
+	active form.
+	"""
 	if title is None:
 		title = getDefaultTitle()
 	icon = wx.ICON_HAND
 	showMessageBox(message=message, title=title, icon=icon, parent=parent)
 
 def info(message="Information", title=None, parent=None):
+	"""Display a dMessageBox offering the user some useful information.
+
+	Returns None.
+
+	The default title comes from app.getAppInfo("appName"), or if the 
+	application object isn't available it will be "Dabo Application".
+
+	If parent isn't passed, it will automatically resolve to the current 
+	active form.
+	"""
 	if title is None:
 		title = getDefaultTitle()
 	icon = wx.ICON_INFORMATION
 	showMessageBox(message=message, title=title, icon=icon, parent=parent)
 
 def exclaim(message="Important!", title=None, parent=None):
+	"""Display a dMessageBox urgently informing the user that we cannot proceed.
+
+	Returns None.
+
+	The default title comes from app.getAppInfo("appName"), or if the 
+	application object isn't available it will be "Dabo Application".
+
+	If parent isn't passed, it will automatically resolve to the current 
+	active form.
+	"""
 	if title is None:
 		title = getDefaultTitle()
 	icon = wx.ICON_EXCLAMATION
@@ -72,16 +115,6 @@ def showMessageBox(message, title, icon, parent=None):
 	style = wx.OK | icon
 	dlg = dMessageBox(message, title, style, parent=parent)
 	dlg.CenterOnParent()
-	retval = dlg.ShowModal()
-	dlg.Destroy()
-	return None
-
-
-def info(message="Information", title=None, parent=None):
-	if title is None:
-		title = getDefaultTitle()
-	style = wx.OK|wx.ICON_INFORMATION
-	dlg = dMessageBox(message, title, style, parent=parent)
 	retval = dlg.ShowModal()
 	dlg.Destroy()
 	return None
