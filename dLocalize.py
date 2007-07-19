fba3f5a7895f70e3a18950fd0a9199c559e16d9d
Ed Leafe <ed@leafe.com>
2007-07-19 00:13:33 +0000

    Re-wrote dLocalize to simplify the way it works. I'm actually able to get newly displayed strings to update, although things such as the menu bar and existing forms will not be affected.
    Added the setLanguage() method to dApp. This will allow apps to change the language that their application's localized strings appear in.
    
    Updated dApp to work with the new interactive web update settings.

--- dLocalize.py
+++ dLocalize.py
@@ -1,78 +1,66 @@
 # -*- coding: utf-8 -*-
-import gettext, os
+import gettext
+import locale
+import os
+import re
 import dabo
-from dabo.__version__ import version
 
-__domain = "dabo"
-__localedir = "locale"
 
-def __translation(domain, version=None, dirs=[]):
-	""" Try to find """
-	dirs = [os.path.join(d, __localedir) for d in dirs]
-	dirs.append(None) # tell to search in system dirs also
-	domains = [domain]
-	version and domains.insert(0, "-".join([domain, version]))
+def getLanguages():
+	# Probably need to add more
+	langs = gettext.find("dabo", daboLocaleDir, languages=_trans.keys(), all=True)
+	langPat = re.compile(".+/locale/([a-z]{2})/LC_MESSAGES.+")
+	ret = [langPat.sub(r"\1", lang) for lang in langs]
+	return ret
 
-	for domain in domains:
-		for dir in dirs:
-			try:
-				return gettext.translation(domain, localedir=dir)
-			except IOError:
-				pass
-	return None
 
-# set default Dabo translation. File is searched under cwd()/locale/$LANG/LC_MESSAGES
-# and in system default places.
-__trans = __translation(__domain, version["version"], ["."]) or gettext.NullTranslations()
-
-__app_initialized = False
-
-def __add_apptrans():
-	global __trans
-	try:
-		app = dabo.dAppRef
-	except AttributeError:
-		app = None
-	if app:
-		appname, appver = [app.getAppInfo(k) for k in ("appName", "appVersion")]
-		if appname:
-			appname = appname.lower()
-			apptrans = __translation(appname, appver, [app.HomeDirectory])
-			if apptrans:
-				apptrans.add_fallback(__trans)
-				__trans = apptrans
-				__app_initialized = True
-
-def _(s):
-	""" Default localization service. Translation is based on default
-	gettext interface. Translation is returned in Unicode.
-
-	Messages are searched first in application translations and in Dabo ones after.
-	
-	Translation files are searched in
-	application.HomeDirectory/locale and system default locale
-	directories accoding to gettext conventions(files must be
-	placed under ${LANG}/LC_MESSAGES subdirectory. File basename must
-	be appName or appName-appVersion in lower case.
-	
-	Default appName is "dabo".
-	"""
-	# application initialized, try to install messages
-	if not __app_initialized:
-		__add_apptrans()
-	
-	app = dabo.dAppRef
-	if isinstance(s, str) and app is not None:
-		ss = app.str2Unicode(s)
+def setLanguage(lang=None, charset=None):
+	global defLang, defCharset, _trans
+	if charset is None:
+		charset = defCharset
+	if lang is None:
+		lang = defLang
 	else:
-		ss = s
-
-	return __trans.ugettext(ss)
-
+		lang = lang.lower()
+		# It might be the full name instead of the two-letter abbreviation
+		if lang not in getLanguages():
+			try:
+				lang = {"english": "en", 
+						"spanish": "es", "espanol": "es", "español": "es",
+						"french": "fr", "francais": "fr", "français": "fr", 
+						"portuguese": "pt", "portuguése": "pt",
+						"russian": "ru"}[lang]
+			except KeyError:
+				pass
+	if not lang in getLanguages():
+		raise IOError, "Invalid language '%s'" % lang
 	
+	if _trans.get(lang) is None:
+		_trans[lang] = gettext.translation("dabo", daboLocaleDir, 
+				languages=[lang], codeset=charset)
+	defLang = lang
+	
+	
+def _(s):
+	global defLang, _trans
+	return _trans[defLang].gettext(s)
+	
+
 def n_(s):
 	""" Use it if you want to tell translation service about string
 	but don't want to translate it inplace.
 	"""
-	return s
+	global defLang
+	return _trans[defLang].gettext(s)
 
+
+daboLocaleDir = os.path.join(os.path.split(dabo.__file__)[0], "locale")
+_trans = {"en": None, "fr": None, "es": None, "pt": None, "ru": None}
+defLang, defCharset = locale.getlocale()
+if defLang is None:
+	defLang = "en"
+else:
+	defLang = defLang[:2]
+if defCharset is None:
+	defCharset = "ISO8859-1"
+setLanguage()
