fea56ee12466693dd3283e4fd598dd279a67c369
Ed Leafe <ed@leafe.com>
2007-09-27 13:20:06 +0000

    Added the PreferenceDialog class. This is the basic form to be used for managing application preferences. You can add your own pages, and each page can have its own method for persisting its settings that will be called if the user clicks 'OK'.

--- ui/dialogs/__init__.py
+++ ui/dialogs/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from about import About
 from login import Login
+from PreferenceDialog import PreferenceDialog
 from SortingForm import SortingForm
 from Wizard import Wizard
 from WizardPage import WizardPage
