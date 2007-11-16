75fe03c6185171c09a5b99e2fe6fb5dd496d0df6
Ed Leafe <ed@leafe.com>
2007-11-16 12:14:06 +0000

    Added an explicit call to self.Stop() to dTimer.release(). I ran into situations where the timer events continued even after the form (and its timer) should have already been destroyed.
    Added a fix that prevents format errors with textboxes that have Mask set from stopping the flow of the program. Instead, an appropriate message is printed to the dabo.errorLog, and execution continues. Thanks to Pedro Vale de Gato for this fix.

--- ui/uiwx/dTextBoxMixin.py
+++ ui/uiwx/dTextBoxMixin.py
@@ -52,7 +52,9 @@ class dTextBoxMixinBase(dcm.dDataControlMixin):
 		setter = self.SetValue
 		if hasattr(self, "ChangeValue"):
 			setter = self.ChangeValue
-		setter(self.getStringValue(self.Value))
+		val = self.getStringValue(self.Value)
+		print "VAL", val
+		setter(val)
 		self.InsertionPosition = insPos
 		self.SelectionStart = startPos
 		self.SelectionEnd = endPos
@@ -659,9 +661,13 @@ class dTextBoxMixin(dTextBoxMixinBase):
 			# Update the display if it is different from what is already displayed
 			# (note that if we did it unconditionally, the user's selection could
 			# be reset, which isn't very nice):
-			if strVal != self.GetValue():
-				setter(strVal)
-		
+			if strVal != _oldVal:
+				try:
+					setter(strVal)
+				except ValueError, e:
+					#PVG: maskedtextedit sometimes fails, on value error..allow the code to continue
+					dabo.errorLog.write(_("Error setting value to '%(strVal)s: %(e)s") % locals())
+
 			if type(_oldVal) != type(val) or _oldVal != val:
 				self._afterValueChanged()
 		else:
