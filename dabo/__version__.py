00bbf4279cf88fa796e5e6df7b13a86c1232b906
Paul McNett <p@ulmcnett.com>
2008-06-03 23:39:18 +0000

    Changelog and version bump to 0.8.4.

--- dabo/__version__.py
+++ dabo/__version__.py
@@ -2,8 +2,8 @@
 # The following 3 lines are the only thing you should change in this file.
 # Everything else is boilerplate copied also to other dabo repositories.
 package_name = "dabo"
-_version = "0.8.3"
-_approximateRevision = "~4105"
+_version = "0.8.4"
+_approximateRevision = "~4111"
 
 import os
 import lib
