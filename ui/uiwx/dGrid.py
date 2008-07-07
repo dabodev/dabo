aacfb65ffb3f19fc09c81db11733d1fc3cf5a35c
Ed Leafe <ed@leafe.com>
2008-07-07 15:56:56 +0000

    Corrected the initial value for dColumn's DataField property.

--- ui/uiwx/dGrid.py
+++ ui/uiwx/dGrid.py
@@ -453,7 +453,7 @@ class dColumn(dabo.ui.dPemMixinBase.dPemMixinBase):
 		self._isConstructed = False
 		self._dynamic = {}
 		# Initialize the data field attribute
-		self._dataField = None
+		self._dataField = ""
 		self._expand = False
 		# Default to 2 decimal places
 		self._precision = 2
