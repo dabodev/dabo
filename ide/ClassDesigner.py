0e871b2c9b4e9b7473f726f0785fd276cee297f1
Ed Leafe <ed@leafe.com>
2008-07-23 00:10:28 +0000

    Reverted the changes in rev. [4300] and [4301], as they should no longer be needed with the dPemMixin change.

--- ide/ClassDesigner.py
+++ ide/ClassDesigner.py
@@ -2600,9 +2600,7 @@ class ClassDesigner(dabo.dApp):
 
 		# Here's where the control is actually created!
 		mixedClass = self.getControlClass(cls)
-		obj = mixedClass(parent, properties=props)
-		if attProperties is not None:
-			obj.setPropertiesFromAtts(attProperties)
+		obj = mixedClass(parent, properties=props, attProperties=attProperties)
 		
 		if issubclass(cls, dui.dTreeView):
 			obj.addDummyData()
