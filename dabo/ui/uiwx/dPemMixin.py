a48f798e34c9a98a045502ffa32186f9a03d5341
Paul McNett <p@ulmcnett.com>
2008-09-09 17:02:14 +0000

    Reverted problematic areas of commit [4510].

--- dabo/ui/uiwx/dPemMixin.py
+++ dabo/ui/uiwx/dPemMixin.py
@@ -208,7 +208,7 @@ class dPemMixin(dPemMixinBase):
 		the given Name isn't unique among siblings.
 		"""
 		if not dabo.fastNameSet:
-			name, _explicitName = self._processName(properties, kwargs, self.__class__.__name__)
+			name, _explicitName = self._processName(kwargs, self.__class__.__name__)
 			self._initName(name, _explicitName=_explicitName)
 
 		# Add any properties that were re-set 
