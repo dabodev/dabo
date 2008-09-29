00221bad2acf912b1e357ff46492205dcf6fbe85
John Fabiani <jfabiani@yolo.com>
2008-09-29 22:42:29 +0000

    wrapped wx.lib.platebtn in wx.version test to fix use with older versions of wxpython

--- dabo/ui/uiwx/__init__.py
+++ dabo/ui/uiwx/__init__.py
@@ -70,7 +70,8 @@ dabo.ui.dSizerMixin = dSizerMixin
 from dBox import dBox
 from dBitmap import dBitmap
 from dBitmapButton import dBitmapButton
-from dBorderlessButton import dBorderlessButton
+if wx.VERSION >= (2, 8, 8):
+	from dBorderlessButton import dBorderlessButton
 from dButton import dButton
 from dCalendar import dCalendar
 from dCalendar import dExtendedCalendar
