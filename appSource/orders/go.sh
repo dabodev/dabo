172b1122fd111df1d92eeaeb8c2524d3a3130ce8
Ed Leafe <ed@leafe.com>
2008-10-05 22:51:41 +0000

    This is the app portion of the Pylons code used to create the server side of a Dabo web application. If you have Pylons installed, this is the second-level directory that is created inside the main directory of the app. Since the main directory contains common distutils stuff, I've not included it here.
    The 'appSource' directory contains a typical datanav application using the dabodev.com public database. I chose this because there still are some unicode issues to work out, and this has lots of non-ASCII values. For your own app, place each app in its own directory under 'appSource'.
    
    The only files you would have to customize are in the daboserver/controllers/ directory. First, you have to add your RemoteBizobj classes there, and then modify bizservers.py to import them. Instructions for customizing bizservers.py are in the comments of that file.

---
+++ appSource/orders/go.sh
@@ -0,0 +1,5 @@
+# go.sh
+# launches the orders app.
+cd /Users/ed/apps/daboserver/daboserver/appSource/orders
+python orders.py Orders
+# python orders.py --OpenAll
---
+++ controllers/OrdersBizobj.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import dabo
+
+class OrdersBizobj(dabo.biz.RemoteBizobj):
+	def defineConnection(self):
+		self.Encoding = "latin-1"
+		self.setConnectionParams(
+				dbType="MySQL", 
+				database="webtest", 
+				host="dabodev.com",
+				user="webuser",
+				plainTextPassword="foxrocks")
+
+
+	def validateRecord(self):
+		"""Place record validation code here"""
+		pass
+			
