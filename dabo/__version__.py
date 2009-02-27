d673d8c6a7b113bac8389eac53b6e7bae9216fb2
Paul McNett <p@ulmcnett.com>
2009-02-27 23:04:02 +0000

    auto-incrementing of revision

--- dabo/__version__.py
+++ dabo/__version__.py
@@ -3,7 +3,7 @@
 # Everything else is boilerplate copied also to other dabo repositories.
 package_name = "dabo"
 _version = "0.9.1"
-_approximateRevision = "~5087"
+_approximateRevision = "~5091"
 
 import os
 import lib
