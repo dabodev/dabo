d05519bec8f6beced61225ddc8ab044836d936d3
Paul McNett <p@ulmcnett.com>
2010-05-26 21:28:41 +0000

    Fixed a bug that would make dDropdownList raise a ValueChanged when the Value was None and Value was programatically changed to None. It didn't change, so we shouldn't raise the event.
    However, this code I commented out has been there a long time, and there must have been
    some reason for it. So I dated and made a comment, in case a problem is discovered.

--- dabo/ui/dDataControlMixinBase.py
+++ dabo/ui/dDataControlMixinBase.py
@@ -196,9 +196,13 @@ class dDataControlMixinBase(dabo.ui.dControlMixin):
 		isChanged = False
 		oldVal = self._oldVal
 
-		if oldVal is None and curVal is None:
-			# Could be changed and we just don't know it...
-			isChanged = True
+		##- pkm 2010-05-26: If oldVal is None and curVal is None, I don't see why we would
+		##-                 think the value was changed. Commenting this out makes 
+		##-                 dDropdownList behave better, in that it doesn't raise ValueChanged
+		##-                 when the oldVal was None and the curVal is None.
+		##- #if oldVal is None and curVal is None:
+		##-	# Could be changed and we just don't know it...
+		##- #	isChanged = True
 		if isinstance(self, (dabo.ui.dToggleButton,)):
 			# These classes change their value before the GotFocus event
 			# can store the oldval, so always flush 'em.
