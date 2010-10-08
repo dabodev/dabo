0206307962c3bdaa80a4632b0ebf3d6ba7426069
Paul McNett <p@ulmcnett.com>
2010-10-08 01:56:08 +0000

    New buildwin.bat to reflect python 2.6 and wxpython 2.8.11.0

--- ide/wizards/AppWizard/spec-buildwin.bat
+++ ide/wizards/AppWizard/spec-buildwin.bat
@@ -1,9 +1,11 @@
 del /q dist
-rmdir /s /q dist
-python -OO setup.py py2exe --bundle 3
+rmdir /s /q dist build
 
-rem You may need to change these paths:
-copy c:\python25\lib\site-packages\wx-2.8-msw-unicode\wx\MSVCP71.dll dist
-copy c:\python25\lib\site-packages\wx-2.8-msw-unicode\wx\gdiplus.dll dist
-copy c:\windows\system32\mfc71.dll dist
+python -OO setup.py py2exe --bundle 1
+
+copy c:\python26\lib\site-packages\wx-2.8-msw-unicode\wx\gdiplus.dll dist
+
+rem  THE FOLLOWING LINE WILL FAIL, until you put msvcr90.dll inside a new win_todist folder
+copy win_todist\msvcr90.dll dist
+
 
