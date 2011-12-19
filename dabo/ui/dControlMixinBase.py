cfad45c7ece55e2a441c0c1c24f69675123ec32a
Jacek Kalucki <laborm@rz.onet.pl>
2011-12-19 11:30:34 +0000

    - Removed unneeded imports. - Moved dForm._activeControl value assignment code after the validation code.

--- dabo/ui/dControlMixinBase.py
+++ dabo/ui/dControlMixinBase.py
@@ -1,27 +1,25 @@
 # -*- coding: utf-8 -*-
 """Provide behavior common to all dControls"""
 
-import dabo
 import dabo.ui
-from dabo.dLocalize import _
 import dabo.dEvents as dEvents
+
 
 class dControlMixinBase(dabo.ui.dPemMixin):
 	"""Provide common functionality for all controls."""
 	def _initEvents(self):
 		super(dControlMixinBase, self)._initEvents()
-
 		self.bindEvent(dEvents.GotFocus, self.__onGotFocus)
 
 	def __onGotFocus(self, evt):
 		if self.Form:
-			  # Grab reference to current ActiveControl
+			# Grab reference to current ActiveControl
 			sfac = self.Form.ActiveControl
 			# Set the form's ActiveControl reference
-			self.Form._activeControl = self
-			if sfac is not None and sfac is not self:
+			if sfac != self:
 				# make sure prior control's value has been flushed
 				self.Form.activeControlValid()
+				self.Form._activeControl = self
 		if self.Parent:
 			self.Parent._activeControl = self
 
