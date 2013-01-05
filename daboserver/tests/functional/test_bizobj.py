7c57619276c8b6f1b01f1b195c14b92e2bffcb99
Paul McNett <paul.mcnett@visumtechnology.com>
2013-01-05 15:35:45 -0800

    Removed sub-projects now in their own repositories.

--- daboserver/tests/functional/test_bizobj.py
+++
@@ -1,7 +0,0 @@
-from daboserver.tests import *
-
-class TestBizobjController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url_for(controller='bizobj'))
-        # Test response...
--- springboard/buildwin.bat
+++
@@ -1,2 +0,0 @@
-python -OO setup.py py2exe --bundle 1
-
