61d3e298119a839399b56f1a2ba672965f204229
EdLeafe <ed@leafe.com>
2013-03-11 17:18:10 -0500

    Found another file with mixed indentation.

--- dabo/lib/connParser.py
+++ dabo/lib/connParser.py
@@ -51,7 +51,7 @@ class connHandler(xml.sax.ContentHandler):
 				# We can now define custom connection parameters, example:
 				# <dialect type="int">1</dialect>
 				# It's an extended connection information, we log it.
-                elem = self.element
+				elem = self.element
 				dabo.log.info(_(u"Extended database connection parameter loaded: "
 						"%(elem)s = %(content)s") % locals())
 				atype = self.attributes.get("type", None)
