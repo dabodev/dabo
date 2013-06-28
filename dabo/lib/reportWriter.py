dabodev/dabo

commit 7e2457a7c66d629aa05aa84b9aa5475d3613f3f0

Fixed recursion error in Werner's test rfxml.
TODO: refactor a lot of the Memo/Paragraph code to be less nested
and to have reportlab doing all the text processing instead of us.

dabo/lib/reportWriter.py
@@ -1675,7 +1675,7 @@ class ReportWriter(object):
 				story = deferred
 				neededHeight = sum([s[1] for s in story])
 			else:
-				story, neededHeight = self.getStory(obj)
+				story, neededHeight = self.getStory(obj, availableHeight=availableHeight)
 
 			dynamicHeight = obj.getProp("Height") is None
 
@@ -1889,7 +1889,7 @@ class ReportWriter(object):
 		return deferred, neededHeight
 
 
-	def getStory(self, obj, overrideExpr=None, overrideFontSize=None):
+	def getStory(self, obj, overrideExpr=None, overrideFontSize=None, availableHeight=None):
 		width = self.getPt(obj.getProp("width"))
 		height = obj.getProp("height")
 		if height is not None:
@@ -1945,11 +1945,12 @@ class ReportWriter(object):
 					s.spaceBefore = fobject.getProp("spaceBefore")
 					s.leftIndent = fobject.getProp("leftIndent")
 				paras = expr.splitlines()
+				rl_paras = []
 				prior_para = ""
 				for para in paras:
 					if len(para) == 0:
 						# Blank line
-						p = platypus.Spacer(0, s.leading)
+						rl_paras.append(platypus.Spacer(0, s.leading))
 					else:
 						def escapePara(para):
 							words = para.split(" ")
@@ -1961,8 +1962,12 @@ class ReportWriter(object):
 								words[idx] = word
 							return " ".join(words)
 						para = escapePara(para)
-						p = ParaClass(para, s)
-					p_height = p.wrap(columnWidth-padLeft-padRight, None)[1]
+						splits = ParaClass(para, s).split(columnWidth-padLeft-padRight, availableHeight)
+						for split in splits:
+							rl_paras.append(split)
+
+				for p in rl_paras:
+					p_height = p.wrap(columnWidth-padLeft-padRight, height)[1]
 					if height is not None and objNeededHeight + p_height > height and not overrideExpr:
 						# We are going to need to truncate the output; find the amount of data we can print
 						# and print that, followed by "..."
@@ -2303,7 +2308,7 @@ class ReportWriter(object):
 				else:
 					for obj in bandDict.get("Objects", []):
 						if "height" in obj and obj.getProp("Height") is None:
-							story = self.getStory(obj)
+							story = self.getStory(obj, availableHeight=availableHeight)
 							storyheight = story[1]
 							needed = storyheight + bandHeight - self.getPt(obj.getProp("y"))  ## y could be dep. on band height.
 							if needed - availableHeight > 30:

commit ac561f6bd4a5d0c480dd3e832c639f676d5e6025

Fix bug introduced in 7e2457a : some Memos were not being printed.
It looks like shorter single-line memos weren't getting printed if
the width of the memo was under some threshold. Maybe I just don't
quite understand reportlab's para.split() function.

dabo/lib/reportWriter.py
@@ -1963,6 +1963,8 @@ class ReportWriter(object):
 							return " ".join(words)
 						para = escapePara(para)
 						splits = ParaClass(para, s).split(columnWidth-padLeft-padRight, availableHeight)
+						if not splits:
+							splits = [ParaClass(para, s)]
 						for split in splits:
 							rl_paras.append(split)
 