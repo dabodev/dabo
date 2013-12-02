dabodev/dabo

commit 7e2457a7c66d629aa05aa84b9aa5475d3613f3f0

Fixed recursion error in Werner's test rfxml.
TODO: refactor a lot of the Memo/Paragraph code to be less nested
and to have reportlab doing all the text processing instead of us.

dabo/lib/reportWriter.py
@@ -1675,7 +1675,7 @@ class ReportWriter(object):
 				story = deferred
 				neededHeight = sum([s[1] for s in story])
 			else:
-				story, neededHeight = self.getStory(obj)
+				story, neededHeight = self.getStory(obj, availableHeight=availableHeight)
 
 			dynamicHeight = obj.getProp("Height") is None
 
@@ -1889,7 +1889,7 @@ class ReportWriter(object):
 		return deferred, neededHeight
 
 
-	def getStory(self, obj, overrideExpr=None, overrideFontSize=None):
+	def getStory(self, obj, overrideExpr=None, overrideFontSize=None, availableHeight=None):
 		width = self.getPt(obj.getProp("width"))
 		height = obj.getProp("height")
 		if height is not None:
@@ -1945,11 +1945,12 @@ class ReportWriter(object):
 					s.spaceBefore = fobject.getProp("spaceBefore")
 					s.leftIndent = fobject.getProp("leftIndent")
 				paras = expr.splitlines()
+				rl_paras = []
 				prior_para = ""
 				for para in paras:
 					if len(para) == 0:
 						# Blank line
-						p = platypus.Spacer(0, s.leading)
+						rl_paras.append(platypus.Spacer(0, s.leading))
 					else:
 						def escapePara(para):
 							words = para.split(" ")
@@ -1961,8 +1962,12 @@ class ReportWriter(object):
 								words[idx] = word
 							return " ".join(words)
 						para = escapePara(para)
-						p = ParaClass(para, s)
-					p_height = p.wrap(columnWidth-padLeft-padRight, None)[1]
+						splits = ParaClass(para, s).split(columnWidth-padLeft-padRight, availableHeight)
+						for split in splits:
+							rl_paras.append(split)
+
+				for p in rl_paras:
+					p_height = p.wrap(columnWidth-padLeft-padRight, height)[1]
 					if height is not None and objNeededHeight + p_height > height and not overrideExpr:
 						# We are going to need to truncate the output; find the amount of data we can print
 						# and print that, followed by "..."
@@ -2303,7 +2308,7 @@ class ReportWriter(object):
 				else:
 					for obj in bandDict.get("Objects", []):
 						if "height" in obj and obj.getProp("Height") is None:
-							story = self.getStory(obj)
+							story = self.getStory(obj, availableHeight=availableHeight)
 							storyheight = story[1]
 							needed = storyheight + bandHeight - self.getPt(obj.getProp("y"))  ## y could be dep. on band height.
 							if needed - availableHeight > 30:

commit 7a0bf5004d943e0f0de0cf2e339cb162e3f6c21c

Squashed commit of the following:
commit a665391bf52c734448afc321315b671fcfdfe422
Author: Paul McNett <p@ulmcnett.com>
Date:   Mon Dec 2 15:07:45 2013 -0800

    Accidentally committed a merge conflict marker

commit 0a8c09bbe060c64f515ea2dd1ac831b9f3b58e84
Merge: 8836d3c 4115095
Author: Paul McNett <p@ulmcnett.com>
Date:   Mon Dec 2 15:00:49 2013 -0800

    Merge branch 'master' into working

    Conflicts:
    	dabo/ui/uiwx/dAutoComplete.py

commit 8836d3c9da183829e8200444379345a1aae40980
Author: johnf <johnf@jfcomputer.com>
Date:   Mon Oct 7 07:28:07 2013 -0700

    added test due to the fact column.DataType was not a string but a subclass

commit dad18a698519dd207daa8088f8d3ca16a63ccfef
Author: EdLeafe <ed@leafe.com>
Date:   Wed Jul 24 09:21:34 2013 -0500

    Stylistic cleanup only. No logic changes.

commit d470c9527dc446d355fe2f6d128385918370cf19
Author: Neil Flowers <njflowers@ucdavis.edu>
Date:   Mon Jul 22 17:01:57 2013 -0700

    Converted dAutoComplete.py to all tab indentation (removed spaces

commit 7465a449d5fe12ab68ff623e27d353ef38b58af7
Author: Neil Flowers <njflowers@ucdavis.edu>
Date:   Mon Jul 22 16:57:32 2013 -0700

    Fix to make sure the dropdown list clears when a datasource updates

commit 7870cc12e8f515243d08270e35a654aa5927c69e
Author: Neil Flowers <njflowers@ucdavis.edu>
Date:   Mon Jul 22 16:33:26 2013 -0700

    Added dAutoComplete, author, import to __init__.py

commit bb8292cfd2d63718061256d244abd3a93a94c0b7
Author: Paul McNett <p@ulmcnett.com>
Date:   Thu Aug 15 19:56:20 2013 -0700

    Added another needed unicode check when logging parameters.

commit 4c7f02fc0aa99c60e7164357dc40995c32cd367a
Author: Paul McNett <p@ulmcnett.com>
Date:   Thu Aug 15 09:07:07 2013 -0700

    Fix SimpleCrypt for newer Python Crypto versions.

    Thanks Jacek!

commit cd6b16b6ee65edfcb5c18ae0489c7c431b8e966d
Author: Paul McNett <p@ulmcnett.com>
Date:   Tue Jul 9 06:23:04 2013 -0700

    Trap unicode error in logging the sql params in execute error.

commit ac561f6bd4a5d0c480dd3e832c639f676d5e6025
Author: Paul McNett <p@ulmcnett.com>
Date:   Fri Jun 28 16:00:16 2013 -0700

    Fix bug introduced in 7e2457a : some Memos were not being printed.

dabo/db/dCursorMixin.py
@@ -324,11 +324,13 @@ class dCursorMixin(dObject):
 				sql = sql.decode(self.Encoding).replace("\n", " ")
 			except UnicodeDecodeError, e:
 				sql = "(couldn't decode sql)"
-		params = ", ".join("%s" % p for p in params)
-		logmsg = "%s SQL: %s, PARAMS: %s" % (msg, sql, params)
+		try:
+			params = ", ".join("%s" % p for p in params)
+		except UnicodeDecodeError, e:
+			params = "(couldn't decode params)"
 
 		try:
-			log(logmsg)
+			log("%s SQL: %s, PARAMS: %s" % (msg, sql, params))
 		except StandardError:
 			log("%s (couldn't log SQL or PARAMS)" % msg)
 
@@ -360,7 +362,7 @@ class dCursorMixin(dObject):
 			# handle it appropriately.
 			if errorClass is not None and isinstance(e, errorClass):
 				raise e
-			self._dblogExecute("execute() FAILED", sql, params) 
+			self._dblogExecute("execute() FAILED", sql, params)
 
 			# Database errors need to be decoded from database encoding.
 			try:

dabo/lib/SimpleCrypt.py
@@ -41,11 +41,13 @@ class SimpleCrypt(object):
 		if self._useDES3:
 			try:
 				from Crypto.Cipher import DES3
+				from Crypto import Random
 			except ImportError:
 				self._useDES3 = False
 		if self._useDES3:
 			self.__key = self.__key[:16].rjust(16, "@")
-			self._cryptoProvider = DES3.new(self.__key, DES3.MODE_CBC)
+			self._cryptoProvider = DES3.new(
+					self.__key, DES3.MODE_CBC, Random.new().read(DES3.block_size))
 
 
 	def showWarning(self):
@@ -80,7 +82,10 @@ class SimpleCrypt(object):
 		if not aString:
 			return ""
 		if self._useDES3:
-			decString = base64.b64decode(aString)
+			try:
+				decString = base64.b64decode(aString)
+			except TypeError:
+				return ""
 			# We need to chop off any padding, along with the first 8 garbage bytes
 			padlen = int(decString[0]) + 8
 			decString = decString[1:]

dabo/lib/reportWriter.py
@@ -1963,6 +1963,8 @@ class ReportWriter(object):
 							return " ".join(words)
 						para = escapePara(para)
 						splits = ParaClass(para, s).split(columnWidth-padLeft-padRight, availableHeight)
+						if not splits:
+							splits = [ParaClass(para, s)]
 						for split in splits:
 							rl_paras.append(split)
 

dabo/ui/uiwx/dAutoComplete.py
@@ -389,7 +389,7 @@ class TextCtrlAutoComplete (wx.TextCtrl, listmix.ColumnSorterMixin):
 				size.SetWidth(width)
 
 			self.dropdown.SetSize(size)
-			self.dropdownlistbox.SetSize(self.dropdown.GetClientSize())	
+			self.dropdownlistbox.SetSize(self.dropdown.GetClientSize())
 
 			if y + size.GetHeight() < self._screenheight :
 				self.dropdown . SetPosition ( wx.Point(x, y) )
@@ -448,7 +448,6 @@ class dAutoComplete(dcm.dControlMixin, TextCtrlAutoComplete):
 		self._baseClass = dAutoComplete
 		preClass = TextCtrlAutoComplete
 		kwargs["entryCallback"] = self.fillDynamicChoices
-		kwargs["selectCallback"] = self._onListItemSelected
 		kwargs["choices"] = [""]
 		self._userColNames = False
 		self._dynamicChoices = None
@@ -469,8 +468,8 @@ class dAutoComplete(dcm.dControlMixin, TextCtrlAutoComplete):
 	def onKeyUp(self,evt):
 		if isinstance(evt, wx.KeyEvent):
 			if evt.GetKeyCode() == wx.WXK_BACK:
-				# Make sure fillDynamicChoices() gets called on backspace as well
-				self.fillDynamicChoices()	   	
+				#Make sure fillDynamicChoices() gets called on backspace as well
+				self.fillDynamicChoices()
 
 
 	def getBizobj(self):
@@ -486,7 +485,7 @@ class dAutoComplete(dcm.dControlMixin, TextCtrlAutoComplete):
 						if not biz.isRemote():
 							self._dataSource = biz
 					return biz
-				form = form.Form	
+				form = form.Form
 		return None
 
 
@@ -519,21 +518,21 @@ class dAutoComplete(dcm.dControlMixin, TextCtrlAutoComplete):
 		choices = []
 		colKeys = self.DataFields
 		if not colKeys:
-			colKeys = [key for key in ds[0].keys()]	
+			colKeys = [key for key in ds[0].keys()]
 
 		if len(colKeys) == 1:
-			# Single column
+			#Single column
 			for rec in ds:
 				choices.append(str(rec[colKeys[0]]))
 		else:
-			# Multi column
+			#Multi column
 			try:
 				for rec in ds:
 					choices.append([str(rec[key]) for key in colKeys])
 			except KeyError:
-				raise ValueError("DataField '%s' is not a valid column name" % key)	
+				raise ValueError("DataField '%s' is not a valid column name" % key)
 
-			# Find search index
+			#Find search index
 			try:
 				if self.SearchField is not None:
 					if isinstance(self.SearchField, basestring):
@@ -545,13 +544,13 @@ class dAutoComplete(dcm.dControlMixin, TextCtrlAutoComplete):
 				raise ValueError("SearchField '%s' is not a valid column name"
 						% self.SearchField)
 
-			# Find fetch index
+			#Find fetch index
 			try:
 				if self.FetchField is not None:
 					if isinstance(self.FetchField, basestring):
 						colFetch = colKeys.index(self.FetchField)
 					else:
-						colFetch = self.FetchField		
+						colFetch = self.FetchField
 				else: colFetch = -1
 			except ValueError:
 				raise ValueError("FetchField '%s' is not a valid column name"
@@ -565,7 +564,7 @@ class dAutoComplete(dcm.dControlMixin, TextCtrlAutoComplete):
 		"""
 		The default entry callback function: takes the user-entered text from
 		the text box and updates the dropdown list to include only entries in
-		which the value of the search field starts with the given string.
+		which the value of the search field starts with the given string.i
 		"""
 		text = self.GetValue().lower()
 		current_choices = self.Choices
@@ -577,7 +576,7 @@ class dAutoComplete(dcm.dControlMixin, TextCtrlAutoComplete):
 			choices = [choice for choice in self._dynamicChoices
 					if choice[self._colSearch].lower().startswith(text)]
 			while len(choices) < 2:
-				choices.append(["" for col in self._dynamicChoices[0]])	
+				choices.append(["" for col in self._dynamicChoices[0]])
 		else:
 			choices = [choice for choice in self._dynamicChoices
 					if choice.lower().startswith(text)]
@@ -595,27 +594,10 @@ class dAutoComplete(dcm.dControlMixin, TextCtrlAutoComplete):
 		the proper number of rows with current data.
 		"""
 		self.listFromDS()
-		
-	def _onListItemSelected(self, event):
-		""" 
-		Raises a dEvents.ListSelection event when a list item is selected. 
-		   eventData will contain a dictionary of ColName:Value pairs, where ColName is derived from
-		   the ColNames property.  If ColNames is not set, column indexes will be used instead.
-		"""
-		eventData = {}
-		if self._colNames:
-			eventData = {self._colNames[i] : name 
-			                     for i, name in enumerate(event) }
-		else:
-			eventData = {i : name 
-			                     for i, name in enumerate(event) }
-				
-		self.raiseEvent(dEvents.ListSelection, eventObject=self,
-		                eventData=eventData)
 
 
 	def _setDynamicChoices(self, choices, colSearch=None, colFetch=None):
-		""" Updates the choices and sets self._dynamicChoices (needed for callback)"""
+		"""Updates the choices and sets self._dynamicChoices (needed for callback)"""
 		self._insertChoices(choices, colSearch, colFetch)
 		self._dynamicChoices = self.Choices
 
@@ -631,8 +613,8 @@ class dAutoComplete(dcm.dControlMixin, TextCtrlAutoComplete):
 			if not colFetch:
 				colFetch = self._colFetch
 			while len(choices) < 2:
-				choices.append(["" for col in choices[0]])		
-			if self._colNames:		
+				choices.append(["" for col in choices[0]])
+			if self._colNames:
 				while len(self._colNames) < len(choices[0]):
 					self._colNames.append("")
 			self.SetMultipleChoices(choices, colSearch=colSearch, colFetch=colFetch)
@@ -712,7 +694,7 @@ class dAutoComplete(dcm.dControlMixin, TextCtrlAutoComplete):
 		if self._constructed():
 			self._searchField = fld
 			if isinstance(fld, int):
-				self._colSearch = fld	
+				self._colSearch = fld
 		else:
 			self._properties["SearchField"] = fld
 
@@ -821,8 +803,7 @@ class dAutoComplete(dcm.dControlMixin, TextCtrlAutoComplete):
 
 			If this list is of a different length than the number of columns,
 			blank column names will be added or extra column names ignored as
-			appropriate.  If a DataSource or DataSet is used, this property will default
-	                to the field names of the data provided.""")
+			appropriate.""")
 
 
 
@@ -855,3 +836,4 @@ if __name__ == "__main__":
 
 
 	test.Test().runTest(TestPanel)
+