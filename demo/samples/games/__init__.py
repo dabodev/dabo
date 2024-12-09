7f0ba85eb9a7e6badc0f435551ec885aab5eef29
EdLeafe <ed@leafe.com>
2024-12-09 10:07:07 -0600

    Removed the demo code that is now in its own repo

--- demo/samples/games/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import bubblet
-from .Minesweeper import MinesweeperForm
-from .Montana import MontanaForm
--- demo/samples/games/bubblet/__init__.py
+++
@@ -1,4 +0,0 @@
-from .BubbleBizobj import BubbleBizobj
-from .BubblePanel import BubblePanel
-from .BubbletForm import BubbletForm
-from .StatsForm import StatsForm
