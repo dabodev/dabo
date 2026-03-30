64bd6014937d1ca8598ff4c537f0a47989fc7393
EdLeafe <ed@leafe.com>
2026-03-29 20:33:03 -0500

    Small improvements to the StopWatch code

--- dabo/lib/StopWatch.py
+++ dabo/lib/StopWatch.py
@@ -1,50 +1,75 @@
-# -*- coding: utf-8 -*-
 import time
 
 
-class StopWatch(object):
+class StopWatch:
     """This is a lightweight stopwatch for timing things."""
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.reset()
+    ZERO_VALUE = 0.00
+
+    def __init__(self, precision=4, *args, **kwargs):
+        """These property setters will all define their underlying attribute"""
+        self.Precision = precision
+        self.Running = False
+        self.StartTime = self.ZERO_VALUE
+        self.Value = self.ZERO_VALUE
 
     def reset(self):
         """Reset the stopwatch timer."""
         self.stop()
-        self._value = 0.00
+        self._value = self.ZERO_VALUE
 
     def start(self):
         """Start the stopwatch if not started already."""
         if not self.Running:
-            self._beg = time.time()
-            self._running = True
+            self.StartTime = time.time()
+            self.Running = True
 
     def stop(self):
         """Stop the stopwatch if not stopped already."""
         if self.Running:
             self._running = False
             end = time.time()
-            self._value += end - self._beg
-            self._beg = 0.00
+            self._value += end - self.StartTime
+            self.StartTime = 0.00
+
+    @property
+    def Precision(self):
+        return self._precision
+
+    @Precision.setter
+    def Precision(self, val):
+        if not isinstance(val, int):
+            raise ValueError("Precision must be an integer")
+        self._precision = val
 
     @property
     def Running(self):
-        try:
-            v = self._running
-        except AttributeError:
-            v = self._running = False
-        return v
+        return self._running
+
+    @Running.setter
+    def Running(self, val):
+        self._running = val
+
+    @property
+    def StartTime(self):
+        return self._start_time
+
+    @StartTime.setter
+    def StartTime(self, val):
+        self._start_time = val
 
     @property
     def Value(self):
-        v = self._value
-
         if self.Running:
             # need to add on the accumulated time.
-            end = time.time()
-            v += end - self._beg
-        return v
+            val = time.time() - self.StartTime
+        else:
+            val = self._value
+        return round(val, self.Precision)
+
+    @Value.setter
+    def Value(self, val):
+        self._value = val
 
 
 if __name__ == "__main__":
@@ -62,7 +87,7 @@ if __name__ == "__main__":
     for i in range(100):
         print(sw.Value, sw.Running)
     sw.reset()
-    print(sw._running, sw._value, sw._beg)
+    print(sw._running, sw._value, sw.StartTime)
     print(sw.Value, sw.Running)
     print("-------3---------")
     sw.start()
